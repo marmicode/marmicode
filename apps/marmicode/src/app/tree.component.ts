86286a74476b1973236f47743ee929337b66d937
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-08 18:29:01 +0200

    🚧 add learning map: integrate amcharts

--- apps/marmicode/src/app/tree.component.ts
+++ apps/marmicode/src/app/tree.component.ts
@@ -2,20 +2,35 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   NgModule,
+  OnDestroy,
   OnInit,
+  ViewChild,
 } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Amcore } from './amcore.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-tree',
-  template: `🚧 tree`,
+  template: `<div #container></div>`,
 })
-export class TreeComponent implements OnInit {
+export class TreeComponent implements OnDestroy, OnInit {
+  @ViewChild('container', { static: true }) containerEl: ElementRef;
+
+  private _subscription: Subscription;
+
   constructor(private _amcore: Amcore) {}
 
   ngOnInit() {
+    this._subscription = this._amcore
+      .createTree(this.containerEl.nativeElement)
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this._subscription.unsubscribe();
   }
 }
 
