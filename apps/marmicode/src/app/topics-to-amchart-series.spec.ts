ca2f5ecd8095978fec94f1861d70dea7a69fc345
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-09 18:18:59 +0200

    🚧 add learning map: convert topics to amchart series

--- apps/marmicode/src/app/topics-to-amchart-series.spec.ts
+++ apps/marmicode/src/app/topics-to-amchart-series.spec.ts
@@ -1,5 +1,40 @@
+interface Topic {
+  id: string;
+  name: string;
+  nextTopics?: string[];
+}
+
+function createTopic(topic: Topic): Topic {
+  return topic;
+}
+
 describe('topicsToAmchartSeries', () => {
   it('🚧 should convert topics list to amchart series', () => {
+    const topics = [
+      createTopic({
+        id: 'web-basics',
+        name: 'Web Basics',
+        nextTopics: ['typescript', 'cli'],
+      }),
+      createTopic({
+        id: 'typescript',
+        name: 'Typescript',
+        nextTopics: ['rxjs'],
+      }),
+      createTopic({
+        id: 'cli',
+        name: 'CLI',
+        nextTopics: ['components'],
+      }),
+      createTopic({
+        id: 'rxjs',
+        name: 'RxJS',
+      }),
+      createTopic({
+        id: 'components',
+        name: 'Components',
+      }),
+    ];
     // create topics list [{id, name, nextTopics}]
     // check function produces amchart series with the right distribution
   });
