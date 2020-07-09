b3da38d71d47ef2c88e6c0fecfffe10aa798bcd7
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-09 18:34:40 +0200

    🚧 add learning map: convert topics to amchart series

--- apps/marmicode/src/app/topics-to-amchart-series.spec.ts
+++ apps/marmicode/src/app/topics-to-amchart-series.spec.ts
@@ -11,61 +11,91 @@ function createTopic(topic: Topic): Topic {
   return topic;
 }
 
+const radius = 60;
+
 /**
  * @deprecated 🚧 Work in progress.
  */
-function topicsToAmchartSeries(topics: Topic[]) {
-  return [];
+function topicsToChartWidth(topics: Topic[]) {
+  return 360;
 }
 
-describe('topicsToAmchartSeries', () => {
-  it('🚧 should convert topics list to amchart series', () => {
-    const topics = [
-      createTopic({
-        id: 'web-basics',
-        name: 'Web Basics',
-        depth: 0,
-        nextTopics: ['typescript', 'cli'],
-      }),
-      createTopic({
-        id: 'typescript',
-        name: 'Typescript',
-        depth: 1,
-        nextTopics: ['rxjs', 'components'],
-      }),
-      createTopic({
-        id: 'cli',
-        name: 'CLI',
-        depth: 1,
-        nextTopics: ['components'],
-      }),
-      createTopic({
-        id: 'components',
-        name: 'Components',
-        depth: 2,
-        nextTopics: ['pipes'],
-      }),
-      createTopic({
-        id: 'rxjs',
-        name: 'RxJS',
-        depth: 3,
-      }),
-    ];
-    /* Radius is 60px and gap is 40px.
-     * Largest row is row 1 and it has 2 items
-     * which means (60 * 2 * 2) + 40 * 3 = 360 */
-    expect(topicsToAmchartSeries(topics)).toEqual([
-      // {
-      //   id: 'web-basics',
-      //   name: 'Web Basics',
-      //   value: 1,
-      //   fixed: true,
-      //   /* Right in the middle. */
-      //   x: 180,
-      //   /* First row so exactly the radius. */
-      //   y: 60,
-      //   linkWith: ['b', 'c'],
-      // },
-    ]);
+/**
+ * @deprecated 🚧 Work in progress.
+ */
+function topicsToChartSeries(topics: Topic[]) {
+  const topic = topics[0];
+  return [
+    {
+      id: topic.id,
+      name: topic.name,
+      value: 1,
+      fixed: true,
+      x: topicsToChartWidth(topics) / 2,
+      y: radius,
+      linkWith: topic.nextTopics,
+    },
+  ];
+}
+
+describe('chart utils', () => {
+  let topics: Topic[];
+
+  beforeEach(
+    () =>
+      (topics = [
+        createTopic({
+          id: 'web-basics',
+          name: 'Web Basics',
+          depth: 0,
+          nextTopics: ['typescript', 'cli'],
+        }),
+        createTopic({
+          id: 'typescript',
+          name: 'Typescript',
+          depth: 1,
+          nextTopics: ['rxjs', 'components'],
+        }),
+        createTopic({
+          id: 'cli',
+          name: 'CLI',
+          depth: 1,
+          nextTopics: ['components'],
+        }),
+        createTopic({
+          id: 'components',
+          name: 'Components',
+          depth: 2,
+          nextTopics: ['pipes'],
+        }),
+        createTopic({
+          id: 'rxjs',
+          name: 'RxJS',
+          depth: 3,
+        }),
+      ])
+  );
+
+  describe('topicsToChartSeries', () => {
+    it('🚧 should convert topics list to amchart series', () => {
+      /* Radius is 60px and gap is 40px.
+       * Largest row is row 1 and it has 2 items
+       * which means (60 * 2 * 2) + 40 * 3 = 360 */
+      expect(topicsToChartSeries(topics)).toEqual([
+        {
+          id: 'web-basics',
+          name: 'Web Basics',
+          value: 1,
+          fixed: true,
+          /* Right in the middle. */
+          x: 180,
+          /* First row so exactly the radius. */
+          y: 60,
+          linkWith: ['typescript', 'cli'],
+        },
+      ]);
+    });
   });
+
+  describe('topicsToChartWidth', () => {});
 });
