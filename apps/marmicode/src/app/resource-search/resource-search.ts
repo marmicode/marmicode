ba71a11c6e985e8126da839d4b8bd5b95b9fd1f0
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-20 12:38:03 +0200

    🚧 add resource search: card flex positioning

--- apps/marmicode/src/app/resource-search/resource-search.ts
+++ apps/marmicode/src/app/resource-search/resource-search.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { ResourceCardModule } from './resource-card.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-resource-search',
   template: `
-    <mc-resource-card></mc-resource-card>
-    <mc-resource-card></mc-resource-card>
+    <section fxLayout="row wrap" fxLayoutAlign="space-around">
+      <mc-resource-card></mc-resource-card>
+      <mc-resource-card></mc-resource-card>
+      <mc-resource-card></mc-resource-card>
+      <mc-resource-card></mc-resource-card>
+      <mc-resource-card></mc-resource-card>
+    </section>
   `,
 })
 export class ResourceSearchComponent {}
@@ -15,6 +21,6 @@ export class ResourceSearchComponent {}
 @NgModule({
   declarations: [ResourceSearchComponent],
   exports: [ResourceSearchComponent],
-  imports: [CommonModule, ResourceCardModule],
+  imports: [CommonModule, ResourceCardModule, FlexLayoutModule],
 })
 export class ResourceSearchModule {}
