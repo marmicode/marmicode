baaa8c7b954fd5b453d148bb2e0ff72fdd6718e2
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-20 15:56:44 +0200

    🚧 add resource search: add workshop type

--- apps/marmicode/src/app/resource-search/resource-type.ts
+++ apps/marmicode/src/app/resource-search/resource-type.ts
@@ -3,13 +3,15 @@ export enum ResourceType {
   Recipe = 'recipe',
   Snack = 'snack',
   Video = 'video',
+  Workshop = 'workshop',
 }
 
 export const resourceTypeColorMap = new Map<ResourceType, string>([
-  [ResourceType.BlogPost, '#800080'],
+  [ResourceType.BlogPost, '#673ab7'],
   [ResourceType.Recipe, '#800080'],
   [ResourceType.Snack, '#800080'],
   [ResourceType.Video, '#800080'],
+  [ResourceType.Workshop, '#673ab7'],
 ]);
 
 export const resourceTypeTextMap = new Map<ResourceType, string>([
@@ -17,4 +19,5 @@ export const resourceTypeTextMap = new Map<ResourceType, string>([
   [ResourceType.Recipe, 'Recipe'],
   [ResourceType.Snack, 'Snack'],
   [ResourceType.Video, 'Video'],
+  [ResourceType.Workshop, 'Workshop'],
 ]);
