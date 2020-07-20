8574cdb801b0030f062c94d5d7801b391146500e
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-20 11:23:57 +0200

    🚧 add resource search: setup angular material

--- apps/marmicode/src/app/app.module.ts
+++ apps/marmicode/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule],
+  imports: [AppRoutingModule, BrowserModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
