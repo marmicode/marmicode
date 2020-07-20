519ce16a43422a30d508eb64712fde7ff2ad7b8f
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-20 18:11:20 +0200

    🚧 add resource search: reuse skills

--- apps/marmicode/src/app/resource-search/skills.ts
+++ apps/marmicode/src/app/resource-search/skills.ts
@@ -1,38 +1,13 @@
 import { createSkill, Skill } from './skill';
 
-export const modulelessAngular = createSkill({
-  id: 'moduleless-angular',
-  label: 'Moduleless Angular',
-});
-
-export const scam = createSkill({
-  id: 'scam',
-  label: 'Single Component Angular Module',
-});
-
-export const angularModules = createSkill({
-  id: 'angular-modules',
-  label: 'Angular Modules',
-});
-
 export const angularLazyLoading = createSkill({
   id: 'angular-lazy-loading',
   label: 'Angular Lazy Loading',
 });
 
-export const limbo = createSkill({
-  id: 'limbo',
-  label: 'Limbo',
-});
-
-export const timeboxedTdd = createSkill({
-  id: 'timeboxed-tdd',
-  label: 'Timeboxed TDD',
-});
-
-export const tcr = createSkill({
-  id: 'tcr',
-  label: 'TCR',
+export const angularModules = createSkill({
+  id: 'angular-modules',
+  label: 'Angular Modules',
 });
 
 export const jasmine = createSkill({
@@ -45,9 +20,45 @@ export const jest = createSkill({
   label: 'Jest',
 });
 
+export const limbo = createSkill({
+  id: 'limbo',
+  label: 'Limbo',
+});
+
+export const modulelessAngular = createSkill({
+  id: 'moduleless-angular',
+  label: 'Moduleless Angular',
+});
+
+export const scam = createSkill({
+  id: 'scam',
+  label: 'Single Component Angular Module',
+});
+
+export const tcr = createSkill({
+  id: 'tcr',
+  label: 'TCR',
+});
+
+export const timeboxedTdd = createSkill({
+  id: 'timeboxed-tdd',
+  label: 'Timeboxed TDD',
+});
+
 export const typescript = createSkill({
   id: 'typescript',
   label: 'Typescript',
 });
 
-export const skills: Skill[] = [];
+export const skills: Skill[] = [
+  angularLazyLoading,
+  angularModules,
+  jasmine,
+  jest,
+  limbo,
+  modulelessAngular,
+  scam,
+  tcr,
+  timeboxedTdd,
+  typescript,
+];
