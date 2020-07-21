b23a7b26ba1f8a3e5aa253d7a2bfc72df2587ac2
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-22 00:34:09 +0200

    🚧 add resource search: use jpg typescript logo

--- apps/marmicode/src/app/resource-search/resources.ts
+++ apps/marmicode/src/app/resource-search/resources.ts
@@ -18,18 +18,6 @@ export const younes = {
   pictureUri: '/assets/authors/younes.jpg',
 };
 
-const typescriptIn5Minutes = createResource({
-  type: ResourceType.Documentation,
-  title: 'TypeScript in 5 minutes',
-  duration: 5,
-  pictureUri: '/assets/resources/typescript-in-5-minutes.png',
-  requiredSkills: [],
-  skills: [typescriptBasics.id],
-  summary: `Unsurprisingly, the official TypeScript documentation contains one of the best introductions to TypeScript.`,
-  url:
-    'https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html',
-});
-
 const boostYourTimeToMarketByDancingTheLimbo = createResource({
   type: ResourceType.Video,
   title: 'Boost Your Time to Market by Dancing the Limbo',
@@ -55,6 +43,18 @@ const yourAngularModuleIsAScam = createResource({
   Choosing the right modules architecture is tricky... but wait, do we really need modules?`,
   url:
     'https://medium.com/marmicode/your-angular-module-is-a-scam-b4136ca3917b',
+});
+
+const typescriptIn5Minutes = createResource({
+  type: ResourceType.Documentation,
+  title: 'TypeScript in 5 minutes',
+  duration: 5,
+  pictureUri: '/assets/resources/typescript.jpg',
+  requiredSkills: [],
+  skills: [typescriptBasics.id],
+  summary: `Unsurprisingly, the official TypeScript documentation contains one of the best introductions to TypeScript.`,
+  url:
+    'https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html',
 });
 
 export const resources = [
