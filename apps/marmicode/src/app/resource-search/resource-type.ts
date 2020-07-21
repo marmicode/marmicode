963d27c24752aa2d50181a2a6824c37344342512
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-21 23:54:13 +0200

    🚧 add resource search: add documentation resource type

--- apps/marmicode/src/app/resource-search/resource-type.ts
+++ apps/marmicode/src/app/resource-search/resource-type.ts
@@ -1,5 +1,6 @@
 export enum ResourceType {
   BlogPost = 'blog-post',
+  Documentation = 'documentation',
   Recipe = 'recipe',
   Snack = 'snack',
   Video = 'video',
@@ -8,6 +9,7 @@ export enum ResourceType {
 
 export const resourceTypeColorMap = new Map<ResourceType, string>([
   [ResourceType.BlogPost, '#66985f'],
+  [ResourceType.Documentation, '#d42020'],
   [ResourceType.Recipe, '#5ab3ad'],
   [ResourceType.Snack, '#f57bb5'],
   [ResourceType.Video, '#eb8053'],
@@ -16,6 +18,7 @@ export const resourceTypeColorMap = new Map<ResourceType, string>([
 
 export const resourceTypeTextMap = new Map<ResourceType, string>([
   [ResourceType.BlogPost, 'Blog Post'],
+  [ResourceType.Documentation, 'Documentation'],
   [ResourceType.Recipe, 'Recipe'],
   [ResourceType.Snack, 'Snack'],
   [ResourceType.Video, 'Video'],
@@ -24,6 +27,7 @@ export const resourceTypeTextMap = new Map<ResourceType, string>([
 
 export const resourceTypeActionMap = new Map<ResourceType, string>([
   [ResourceType.BlogPost, 'Read'],
+  [ResourceType.Documentation, 'Read'],
   [ResourceType.Recipe, `Let's cook!`],
   [ResourceType.Snack, `Let's eat!`],
   [ResourceType.Video, 'Watch'],
