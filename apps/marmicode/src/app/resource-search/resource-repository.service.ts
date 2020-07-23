5a88a649d49c2388a800f0d6192d13aef0dd06c8
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-24 01:29:46 +0200

    🚧 load resources from contentful: generate schema & types

--- apps/marmicode/src/app/resource-search/resource-repository.service.ts
+++ apps/marmicode/src/app/resource-search/resource-repository.service.ts
@@ -1,7 +1,10 @@
 import { Injectable, NgModule } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { map } from 'rxjs/operators';
 import { GraphQLModule } from '../graphql.module';
+import { Query } from '../graphql/schema';
+import { createResource, Resource } from './resource';
 
 const AllResourcesQuery = gql`
   query Resources {
@@ -27,9 +30,18 @@ export class ResourceRepository {
   constructor(private _apollo: Apollo) {}
 
   getResources() {
-    return this._apollo.query({
+    return this._apollo.query<Query>({
       query: AllResourcesQuery,
     });
+    // .pipe(
+    //   map(({ data }) => {
+    //     return data.resourceCollection.items.map((item) => {
+    //       return createResource({
+    //         id: item.sys.id,
+    //       });
+    //     });
+    //   })
+    // );
   }
 }
 
