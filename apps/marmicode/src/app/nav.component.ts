c0a63f15139ad52bc34d8680d5c849ffdc59d6d7
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-23 19:46:23 +0200

    ✅ remove sidenav

--- apps/marmicode/src/app/nav.component.ts
+++ apps/marmicode/src/app/nav.component.ts
@@ -16,51 +16,16 @@ import { map, shareReplay } from 'rxjs/operators';
   template: `
     <!-- Toolbar. -->
     <mat-toolbar color="primary">
-      <button
-        type="button"
-        aria-label="Toggle sidenav"
-        mat-icon-button
-        (click)="toggleSidenav()"
-        *ngIf="isHandset$ | async"
-      >
-        <mat-icon aria-label="Side nav toggle icon">menu</mat-icon>
-      </button>
-      <ng-container *ngIf="(isHandset$ | async) === false">
+      <ng-container>
         <img height="40" src="/assets/logo-white.svg" />
         <span fxFlexAlign="end" class="title">Marmicode</span>
       </ng-container>
     </mat-toolbar>
 
-    <mat-sidenav-container class="sidenav-container">
-      <!-- Sidenav. -->
-      <mat-sidenav
-        class="sidenav"
-        [attr.role]="(isHandset$ | async) ? 'dialog' : 'navigation'"
-        [mode]="(isHandset$ | async) ? 'over' : 'side'"
-        [opened]="isSidenavOpen$ | async"
-      >
-        <mat-nav-list (click)="closeSidenav()">
-          <a mat-list-item routerLink="/learning-map">Learning Map</a>
-          <a mat-list-item routerLink="/search">Search</a>
-        </mat-nav-list>
-      </mat-sidenav>
-
-      <!-- Content. -->
-      <mat-sidenav-content>
-        <ng-content></ng-content>
-      </mat-sidenav-content>
-    </mat-sidenav-container>
+    <ng-content></ng-content>
   `,
   styles: [
     `
-      .sidenav-container {
-        height: calc(100% - 64px);
-      }
-
-      .sidenav {
-        width: 200px;
-      }
-
       .title {
         margin-left: 15px;
         margin-bottom: 2px;
@@ -68,40 +33,7 @@ import { map, shareReplay } from 'rxjs/operators';
     `,
   ],
 })
-export class NavComponent {
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
-  /* State of sidenav. */
-  isSidenavOpen$: Observable<boolean>;
-  /* Internal state of sidenav. */
-  private _isSidenavOpen$ = new BehaviorSubject<boolean>(false);
-
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.isSidenavOpen$ = combineLatest([
-      this._isSidenavOpen$,
-      this.isHandset$,
-    ]).pipe(
-      map(([isSidenavOpen, isHandset]) => {
-        if (!isHandset) {
-          return true;
-        }
-        return isSidenavOpen;
-      })
-    );
-  }
-
-  closeSidenav() {
-    this._isSidenavOpen$.next(false);
-  }
-
-  toggleSidenav() {
-    this._isSidenavOpen$.next(!this._isSidenavOpen$.value);
-  }
-}
+export class NavComponent {}
 
 @NgModule({
   declarations: [NavComponent],
