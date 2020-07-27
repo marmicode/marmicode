49ba7b6765eb52138ad6a12081b99bcd7ed366ed
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-27 14:45:43 +0200

    🚧 search resources by skill: add <mc-skill-chip>

--- libs/resource-feature-search/src/lib/skill-chip.component.ts
+++ libs/resource-feature-search/src/lib/skill-chip.component.ts
@@ -6,22 +6,44 @@ import {
   NgModule,
 } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatRippleModule } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
+import { resourceSearchRouterHelper } from '@marmicode/resource-feature-search';
 import { Skill } from './skill';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-skill-chip',
-  template: ` <mat-chip [disabled]="true" [style.opacity]="1">
-    {{ skill.label }}
-  </mat-chip>`,
+  template: `
+    <a class="chip" [routerLink]="resourceSearchRouterHelper.learn(skill.slug)">
+      {{ skill.label }}
+    </a>
+  `,
+  styles: [
+    `
+      .chip {
+        background-color: #f0f0f0;
+        border-radius: 16px;
+        color: black;
+        cursor: pointer;
+        font-weight: 500;
+        margin: 4px;
+        min-height: 32px;
+        padding: 7px 12px;
+        text-decoration: none;
+      }
+    `,
+  ],
 })
 export class SkillChipComponent {
   @Input() skill: Skill;
+
+  resourceSearchRouterHelper = resourceSearchRouterHelper;
 }
 
 @NgModule({
   declarations: [SkillChipComponent],
   exports: [SkillChipComponent],
-  imports: [CommonModule, MatChipsModule],
+  imports: [CommonModule, RouterModule],
 })
 export class SkillChipModule {}
