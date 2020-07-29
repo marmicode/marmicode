b43dfaead09920105ccdffe5e5755cc94e8afb00
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-29 19:54:51 +0200

    🚧 sync route with search form: setup ngrx
    ng g @nrwl/angular:ngrx app --module=apps/marmicode/src/app/app.module.ts --root

--- apps/marmicode/src/app/app.module.ts
+++ apps/marmicode/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { AppComponent } from './app.component';
 import { NavModule } from './nav.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,10 +19,23 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule.withServerTransition({ appId: 'marmicode' }),
+    EffectsModule.forRoot([]),
     HttpClientModule,
     LayoutModule,
     NavModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+    }),
+    StoreModule.forRoot(
+      {},
+      {
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   bootstrap: [AppComponent],
 })
