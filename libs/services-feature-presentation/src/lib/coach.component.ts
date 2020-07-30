87d6ab47f1c358107c6f3c27317c0c4305bad5a9
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-30 16:58:42 +0200

    🚧 add services presentation: positioning coach picture

--- libs/services-feature-presentation/src/lib/coach.component.ts
+++ libs/services-feature-presentation/src/lib/coach.component.ts
@@ -1,5 +1,9 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { shareReplayWithRefCount } from '@marmicode/shared-utils';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { DottyLineModule } from './dotty-line.component';
 
 declare var require;
@@ -7,11 +11,25 @@ declare var require;
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-coach',
-  template: `<section class="coach-container">
-    <img [src]="coachPictureUri" [alt]="name" class="coach-picture" />
+  template: `<section
+    [style.marginTop.px]="pictureRadius$ | async"
+    [style.paddingTop.px]="(isDesktop$ | async) ? 30 : 10"
+    class="coach-container"
+  >
+    <img
+      [src]="coachPictureUri"
+      [alt]="name"
+      [style.top.px]="-(pictureRadius$ | async)"
+      [style.right]="picturePosition$ | async"
+      [style.height.px]="2 * (pictureRadius$ | async)"
+      [style.width.px]="2 * (pictureRadius$ | async)"
+      class="coach-picture"
+    />
     <h2 class="coach-name">{{ name }}</h2>
-    <p>Google Developer Expert<br />for Angular & Web Technologies</p>
-    <p>eXtreme Programming Coach</p>
+    <p class="coach-title">
+      Google Developer Expert<br />for Angular & Web Technologies
+    </p>
+    <p class="coach-title">eXtreme Programming Coach</p>
     <mc-dotty-line></mc-dotty-line>
     <p>
       Younes is a trainer, consultant & eXtreme Programming coach who loves the
@@ -33,8 +51,6 @@ declare var require;
   styles: [
     `
       .coach-container {
-        margin-top: 80px;
-        padding-top: 10px;
         padding-left: 20px;
         padding-right: 20px;
         position: relative;
@@ -46,10 +62,6 @@ declare var require;
 
       .coach-picture {
         position: absolute;
-        top: -80px;
-        right: -3px;
-        height: 160px;
-        width: 160px;
         border-radius: 50%;
         border-color: var(--marmicode-accent-color);
         border-style: solid;
@@ -60,12 +72,34 @@ declare var require;
         font-size: 1.5em;
         font-weight: 800;
       }
+
+      .coach-title {
+        font-size: 1.2em;
+      }
     `,
   ],
 })
 export class CoachComponent {
   coachPictureUri = require('!!file-loader!./coach.jpg').default;
   name = 'Younes Jaaidi';
+  isDesktop$ = this._breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map((result) => !result.matches),
+    shareReplayWithRefCount()
+  );
+  picturePosition$: Observable<string>;
+  pictureRadius$: Observable<number>;
+
+  constructor(private _breakpointObserver: BreakpointObserver) {
+    const desktopPictureWidth = 100;
+    this.pictureRadius$ = this.isDesktop$.pipe(
+      map((isDesktop) => (isDesktop ? desktopPictureWidth : 80))
+    );
+    this.picturePosition$ = this.isDesktop$.pipe(
+      map((isDesktop) =>
+        isDesktop ? `calc(50% - ${desktopPictureWidth + 3}px)` : '-3px'
+      )
+    );
+  }
 }
 
 @NgModule({
