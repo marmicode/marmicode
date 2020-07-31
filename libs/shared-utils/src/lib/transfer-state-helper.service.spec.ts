bd6b7db7b12e70471deb5e91b9731a754ca76103
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-31 10:07:28 +0200

    🚧 setup scully: fix state transfer pollution

--- libs/shared-utils/src/lib/transfer-state-helper.service.spec.ts
+++ libs/shared-utils/src/lib/transfer-state-helper.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { TransferStateAdapter } from './transfer-state-adapter.service';
 import { TransferStateHelper } from './transfer-state-helper.service';
 
@@ -25,11 +26,30 @@ describe('transferStateHelper', () => {
   let helper: TransferStateHelper;
   beforeEach(() => (helper = TestBed.inject(TransferStateHelper)));
 
+  let observer: jest.Mock;
+  beforeEach(() => (observer = jest.fn()));
+
   describe('when prerendering', () => {
+    beforeEach(() => {
+      (adapter.isPrerendering as jest.Mock).mockReturnValue(true);
+    });
+
+    it('should update state transfer', () => {
+      const source$ = of(42);
+
+      source$.pipe(helper.transfer('theAnswer')).subscribe(observer);
+
+      expect(observer).toBeCalledTimes(1);
+      expect(observer).toBeCalledWith(42);
+      expect(adapter.set).toBeCalledWith('theAnswer', 42);
+    });
+  });
+
+  describe('on browser', () => {
     beforeEach(() => {
       (adapter.isPrerendering as jest.Mock).mockReturnValue(false);
     });
 
-    xit('🚧 should update state transfer', () => {});
+    it.todo('🚧 should read state');
   });
 });
