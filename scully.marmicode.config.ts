957e77355e267775d3143e2eb6308a87df30dd74
Younes Jaaidi <yjaaidi@gmail.com>
2020-07-31 12:27:21 +0200

    🚧 setup scully: fetch all routes

--- scully.marmicode.config.ts
+++ scully.marmicode.config.ts
@@ -1,11 +1,27 @@
-import { RouteTypes, ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig } from '@scullyio/scully';
+import axios from 'axios';
 
 export const config: ScullyConfig = {
   outDir: 'dist/apps/marmicode/static',
-  routes: {
-    '/services': {
-      type: RouteTypes.default,
-    },
-  },
-  extraRoutes: ['/learn/everything'],
+  extraRoutes: getExtraRoutes(),
 };
+
+async function getExtraRoutes() {
+  return ['/services', ...(await getLearnBySkillRoutes())];
+}
+
+async function getLearnBySkillRoutes() {
+  const { data } = await axios.get(
+    'https://cdn.contentful.com/spaces/gowvxq3b4aid/environments/master/entries?content_type=skill&select=fields.slug',
+    {
+      headers: {
+        Authorization: `Bearer _d7Vb4r2lborp1uLTvsv0ne7fB0OMez2FscTnHQux5A`,
+      },
+    }
+  );
+  const skillSlugs = [
+    'everything',
+    ...data.items.map((item) => item.fields.slug),
+  ];
+  return skillSlugs.map((slug) => `/learn/${slug}`);
+}
