0033bc8092ba9a801f4c8d62961a67b2e6e8f1a3
Younes Jaaidi <yjaaidi@gmail.com>
2020-08-03 20:15:25 +0200

    ✅ add deploy:next & deploy:prod scripts

---
+++ tools/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# Exit on error.
+set -e
+
+PROJECT=$1
+
+if [ -z "$PROJECT" ]
+then
+  USAGE="
+Usage: $(basename $0) PROJECT
+"
+  echo "$USAGE"
+  exit 1
+fi
+
+yarn build marmicode --prod
+
+if [ "$PROJECT" = "marmicode-next" ]
+then
+  cp apps/marmicode/src/robots-next.txt dist/apps/marmicode/browser/robots.txt
+fi
+
+yarn prerender:scully
+
+firebase --project "$PROJECT" deploy
