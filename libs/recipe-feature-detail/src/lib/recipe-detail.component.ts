04f456821fe480968942f3ceaf8fbb75131a7f9a
Younes Jaaidi <yjaaidi@gmail.com>
2020-08-04 13:32:18 +0200

    🚧 show recipe with timeline

--- libs/recipe-feature-detail/src/lib/recipe-detail.component.ts
+++ libs/recipe-feature-detail/src/lib/recipe-detail.component.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { RxState } from '@rx-angular/state';
+import { Recipe, RecipeRepository } from './recipe-repository.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-recipe-detail',
-  template: `🚧 recipe-detail`,
+  template: `{{ recipe$ | async | json }}`,
+  providers: [RxState],
 })
-export class RecipeDetailComponent {}
+export class RecipeDetailComponent {
+  recipe$ = this._state.select('recipe');
+
+  constructor(
+    private _recipeRepository: RecipeRepository,
+    private _state: RxState<{ recipe: Recipe }>
+  ) {
+    this._state.connect('recipe', this._recipeRepository.getRecipe());
+  }
+}
 
 @NgModule({
   declarations: [RecipeDetailComponent],
