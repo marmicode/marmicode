d8b860acb3bd1783a31e90c72fb9b41c68cc73e9
Younes Jaaidi <yjaaidi@gmail.com>
2020-08-05 14:41:03 +0200

    🚧 show recipe with timeline: add link to recipe

--- libs/shared-ui/src/lib/link.component.ts
+++ libs/shared-ui/src/lib/link.component.ts
@@ -5,21 +5,31 @@ import {
   Input,
   NgModule,
 } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-link',
-  template: `<a [href]="href" target="_blank">
-    <ng-content></ng-content>
-  </a>`,
+  template: `<a *ngIf="href" [href]="href" target="_blank">
+      <ng-container *ngTemplateOutlet="templateRef"></ng-container>
+    </a>
+
+    <a *ngIf="route" [routerLink]="route">
+      <ng-container *ngTemplateOutlet="templateRef"></ng-container>
+    </a>
+
+    <ng-template #templateRef>
+      <ng-content></ng-content>
+    </ng-template> `,
 })
 export class LinkComponent {
   @Input() href: string;
+  @Input() route: string[];
 }
 
 @NgModule({
   declarations: [LinkComponent],
   exports: [LinkComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class LinkModule {}
