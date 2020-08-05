f9f0596ef01f7bfacfc8b8b5451758a80a3e75c6
Younes Jaaidi <yjaaidi@gmail.com>
2020-08-05 03:00:08 +0200

    🚧 show recipe with timeline: add title

--- libs/recipe-feature-detail/src/lib/recipe-title.component.ts
+++ libs/recipe-feature-detail/src/lib/recipe-title.component.ts
@@ -5,17 +5,52 @@ import {
   Input,
   NgModule,
 } from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-recipe-title',
-  template: `<h1>{{ title }}</h1>`,
+  template: ` <div fxLayout="row" fxLayoutAlign="center">
+      <div class="badge">RECIPE</div>
+    </div>
+    <h1 class="title">{{ title }}</h1>
+    <svg preserveAspectRatio="none" viewBox="0 0 100 100">
+      <polygon style="fill: #561f4b" points="0,0 20,0 10,100 0,100" />
+    </svg>`,
   styles: [
     `
       :host {
-        background-color: #561f4b;
+        position: relative;
+        background-color: #86527c;
+      }
+
+      .badge {
+        position: relative;
+        border-radius: 10px;
+        height: 20px;
+        margin-top: 10px;
+        padding: 0 10px;
+        z-index: 1;
+
+        background-color: #5ab3ad;
+        color: #561f4b;
+        box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.2);
+      }
+
+      .title {
+        position: relative;
         color: white;
+        font-weight: 300;
+        margin: 10px 0;
         text-align: center;
+        z-index: 1;
+      }
+
+      svg {
+        position: absolute;
+        top: 0;
+        height: 100%;
+        width: 100%;
       }
     `,
   ],
@@ -27,6 +62,6 @@ export class RecipeTitleComponent {
 @NgModule({
   declarations: [RecipeTitleComponent],
   exports: [RecipeTitleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FlexModule],
 })
 export class RecipeTitleModule {}
