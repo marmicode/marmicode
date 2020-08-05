71d042e94402c9019679aae930c233cabe0d08ff
Younes Jaaidi <yjaaidi@gmail.com>
2020-08-06 00:05:01 +0200

    🚧 prompt user to reload

--- apps/marmicode/src/app/update/update-dialog.component.ts
+++ apps/marmicode/src/app/update/update-dialog.component.ts
@@ -1,16 +1,56 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { A11yModule } from '@angular/cdk/a11y';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Inject,
+  NgModule,
+} from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-update-dialog',
-  template: `🚧 update-dialog`,
+  template: ` <h1 mat-dialog-title>New version available</h1>
+    <div mat-dialog-content>
+      <p>Marmicode has been updated, are you ready to reload?</p>
+    </div>
+    <div mat-dialog-actions cdkTrapFocus fxLayout="row" fxLayoutAlign="center">
+      <button mat-button mat-dialog-close>LATER</button>
+      <button
+        (click)="update()"
+        mat-raised-button
+        color="primary"
+        cdkFocusInitial
+      >
+        RELOAD
+      </button>
+    </div>`,
 })
-export class UpdateDialogComponent {}
+export class UpdateDialogComponent {
+  constructor(
+    private _swUpdate: SwUpdate,
+    @Inject(DOCUMENT) private _document: Document
+  ) {}
+
+  async update() {
+    await this._swUpdate.activateUpdate();
+    this._document.location.reload();
+  }
+}
 
 @NgModule({
   declarations: [UpdateDialogComponent],
   exports: [UpdateDialogComponent],
-  imports: [CommonModule],
+  imports: [
+    A11yModule,
+    CommonModule,
+    MatButtonModule,
+    MatDialogModule,
+    FlexModule,
+  ],
 })
 export class UpdateDialogModule {}
