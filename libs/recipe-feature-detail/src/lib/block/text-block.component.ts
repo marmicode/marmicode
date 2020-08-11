365413465a7950a5b1ab02804e7e56697f808d95
Younes Jaaidi <yjaaidi@gmail.com>
2020-08-11 16:01:51 +0200

    ✅ style text

--- libs/recipe-feature-detail/src/lib/block/text-block.component.ts
+++ libs/recipe-feature-detail/src/lib/block/text-block.component.ts
@@ -17,6 +17,15 @@ import { MarkdownPipeModule } from './markdown.pipe';
       :host {
         display: block;
         margin: 10px;
+
+        color: #292929;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-size: 21px;
+        font-weight: 400;
+        line-height: 32px;
+        text-rendering: optimizeLegibility;
+        word-break: break-word;
+        -webkit-font-smoothing: antialiased;
       }
     `,
   ],
