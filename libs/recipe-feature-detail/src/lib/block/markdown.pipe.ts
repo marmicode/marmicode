b7e0ad08f7d7dc36b906d4033d3e50ce0aaefa1f
Younes Jaaidi <yjaaidi@gmail.com>
2020-09-25 16:57:55 +0200

    ✅ open markdown links in new tab

--- libs/recipe-feature-detail/src/lib/block/markdown.pipe.ts
+++ libs/recipe-feature-detail/src/lib/block/markdown.pipe.ts
@@ -2,21 +2,34 @@ import { CommonModule } from '@angular/common';
 import {
   NgModule,
   Pipe,
-  PipeTransform,
-  Sanitizer,
-  SecurityContext,
+  PipeTransform
 } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import * as marked from 'marked';
+import { Renderer } from 'marked';
 
 @Pipe({
   name: 'markdown',
 })
 export class MarkdownPipe implements PipeTransform {
-  constructor(private _sanitizer: DomSanitizer) {}
+  private _renderer: Renderer;
+
+  constructor(private _sanitizer: DomSanitizer) {
+    const renderer = new Renderer();
+    const renderLink = renderer.link.bind(renderer);
+    renderer.link = (href, title, text) => {
+      const html = renderLink(href, title, text);
+      return html.replace(/^<a /, '<a target="_blank"');
+    };
+    this._renderer = renderer;
+  }
 
   transform(value: string): SafeHtml {
-    return this._sanitizer.bypassSecurityTrustHtml(marked(value));
+    return this._sanitizer.bypassSecurityTrustHtml(
+      marked(value, {
+        renderer: this._renderer,
+      })
+    );
   }
 }
 
