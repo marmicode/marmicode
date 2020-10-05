a81e8cc930d18c9c8bb75f78f5280f73693e5322
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-05 17:51:09 +0200

    📦 run nx migrations
    nx migrate --run-migrations=migrations.json

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html'],
+  projects: [
+    '<rootDir>/apps/marmicode',
+    '<rootDir>/libs/learning-map-feature-tree',
+    '<rootDir>/libs/resource-feature-search',
+    '<rootDir>/libs/shared-ui',
+    '<rootDir>/libs/shared-utils',
+    '<rootDir>/libs/services-feature-presentation',
+    '<rootDir>/libs/shared-router-helpers',
+    '<rootDir>/libs/recipe-feature-detail',
+    '<rootDir>/libs/resource-core',
+    '<rootDir>/libs/resource-api',
+    '<rootDir>/libs/contentful-api',
+  ],
 };
--- libs/recipe-feature-detail/jest.config.js
+++ libs/recipe-feature-detail/jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  name: 'recipe-feature-detail',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ],
+      },
     },
   },
   coverageDirectory: '../../coverage/libs/recipe-feature-detail',
@@ -18,4 +19,5 @@ module.exports = {
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
+  displayName: 'recipe-feature-detail',
 };
