42e950a23d10ae7089c6f772b4530f92cdc6a5e3
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-14 16:34:43 +0200

    🐞 fix visual regression testing by getting rid of shadow dom

--- libs/recipe-ui/src/lib/block/text-block.component.ts
+++ libs/recipe-ui/src/lib/block/text-block.component.ts
@@ -11,38 +11,10 @@ import { MarkdownPipeModule } from './markdown.pipe';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
   selector: 'mc-text-block',
   template: `<div [innerHTML]="block.text | markdown"></div>`,
-  encapsulation: ViewEncapsulation.ShadowDom,
-  styles: [
-    `
-      :host {
-        display: block;
-        margin: 10px;
-
-        color: #292929;
-        font-family: Georgia, 'Times New Roman', Times, serif;
-        font-size: 17px;
-        font-weight: 400;
-        line-height: 32px;
-        text-rendering: optimizeLegibility;
-        word-break: break-word;
-        -webkit-font-smoothing: antialiased;
-      }
-
-      @media (min-width: 960px) {
-        :host {
-          font-size: 20px;
-        }
-      }
-
-      code {
-        background-color: #eee;
-        border-radius: 3px;
-        padding: 0 2px;
-      }
-    `,
-  ],
+  styleUrls: ['./text-block.component.scss'],
 })
 export class TextBlockComponent {
   @Input() block: TextBlock;
