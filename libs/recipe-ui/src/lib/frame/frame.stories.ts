ded571bc7f879ced947d299862dd49b6fe199464
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-14 10:52:31 +0200

    🚧 add frame visual regression test

--- libs/recipe-ui/src/lib/frame/frame.stories.ts
+++ libs/recipe-ui/src/lib/frame/frame.stories.ts
@@ -1,3 +1,4 @@
+import { BlockType, createFrame } from '@marmicode/recipe-core';
 import { FrameComponent, FrameModule } from './frame.component';
 
 export default {
@@ -9,4 +10,24 @@ export const main = () => ({
     imports: [FrameModule],
   },
   component: FrameComponent,
+  props: {
+    frame: createFrame({
+      blocks: [
+        {
+          type: BlockType.Text,
+          text:
+            "Before setting up request validation, we can see that:\n- the farm detail `GET /farms/{farmId}` route is accessible even though it's not defined in the OpenAPI Specification,\n- we can send data in any format and not only `application/json` as described in the OpenAPI Specification,\n- we can send any data when creating a farm using the `POST /farms` route.",
+        },
+        {
+          type: BlockType.Code,
+          code:
+            '# Get a farm.\ncurl http://localhost:8080/farms/P4VU2Xsw\n\n# Create a farm with urlencoded data\ncurl http://localhost:8080/farms -d"name=springfield"\n\n# Create a farm with invalid data.\ncurl http://localhost:8080/farms \\\n  -H "Content-Type: application/json" \\\n  -d \'{"name": 123, "random": "data"}\'\n\nblalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla ',
+          language: 'shell',
+        },
+      ],
+      duration: 2,
+      slug: 'without-validation',
+      title: 'Without validation',
+    }),
+  },
 });
