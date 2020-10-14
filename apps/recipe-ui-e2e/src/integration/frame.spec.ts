e6d89a65ee1cb344684d645671bfb2e5d20a872e
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-14 16:34:59 +0200

    🐞 fix visual regression testing by getting rid of shadow dom

--- apps/recipe-ui-e2e/src/integration/frame.spec.ts
+++ apps/recipe-ui-e2e/src/integration/frame.spec.ts
@@ -1,12 +1,8 @@
 describe('FrameComponent', () => {
   it('should apply horizontal scroll if code overflows', () => {
     cy.visit('/iframe.html?id=frame--overflow');
-    cy.get('[data-role="code-block"]', { includeShadowDom: true }).should(
-      'contain',
-      '# Get a farm.'
-    );
+    cy.get('[data-role="code-block"]').should('contain', '# Get a farm.');
     cy.percySnapshot(undefined, {
-      enableJavaScript: true,
       widths: [360, 768, 1280],
     });
   });
