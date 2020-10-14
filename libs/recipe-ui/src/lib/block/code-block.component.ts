0fb6dd2369d7d11ddf580cfb2d7973f150184ca9
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-14 16:31:38 +0200

    🐞 fix visual regression testing by getting rid of shadow dom

--- libs/recipe-ui/src/lib/block/code-block.component.ts
+++ libs/recipe-ui/src/lib/block/code-block.component.ts
@@ -11,17 +11,13 @@ import { CodePipeModule } from './code.pipe';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.ShadowDom,
+  encapsulation: ViewEncapsulation.None,
   selector: 'mc-code-block',
   template: `<pre
     [ngClass]="'language-' + block.language"
     class="preformatted"
   ><code [innerHTML]="block.code | code:{language: block.language}" class="code"  data-role="code-block"></code></pre>`,
-  styleUrls: [
-    /* @hack storybook doesn't support styles + styleUrls combination. */
-    './code-block.component.css',
-    '../../../../../node_modules/prismjs/themes/prism-tomorrow.css',
-  ],
+  styleUrls: ['./code-block.component.scss'],
 })
 export class CodeBlockComponent {
   @Input() block: CodeBlock;
