696f71bca9e6daaaa43668d7a2571609fb56d3ef
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-17 13:49:38 +0200

    🚧 highlight lines on text click or hover

--- libs/recipe-ui/src/lib/block/code-block.component.ts
+++ libs/recipe-ui/src/lib/block/code-block.component.ts
@@ -2,25 +2,39 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   Input,
   NgModule,
+  OnChanges,
+  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 import { CodeBlock } from '@marmicode/recipe-core';
+import * as Prism from 'prismjs';
 import { CodePipeModule } from './code.pipe';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   selector: 'mc-code-block',
-  template: `<pre
-    [ngClass]="'language-' + block.language"
-    class="preformatted"
-  ><code [innerHTML]="block.code | code:{language: block.language}" class="code"  data-role="code-block"></code></pre>`,
+  template: `<pre #code class="preformatted"><code
+    class="code"  
+    data-role="code-block"></code></pre>`,
   styleUrls: ['./code-block.component.scss'],
 })
-export class CodeBlockComponent {
+export class CodeBlockComponent implements OnChanges {
   @Input() block: CodeBlock;
+
+  @ViewChild('code', { static: true }) codeEl: ElementRef<HTMLElement>;
+
+  ngOnChanges() {
+    // @todo use renderer.
+    // @todo add line numbers.
+    // @todo highlight lines.
+    this.codeEl.nativeElement.classList.add(`language-${this.block.language}`);
+    this.codeEl.nativeElement.textContent = this.block.code;
+    Prism.highlightElement(this.codeEl.nativeElement);
+  }
 }
 
 @NgModule({
