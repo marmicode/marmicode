16bd92a28e3f5e5624273a6551b378d15fdd8c24
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-28 17:49:41 +0100

    🚧 highlight lines on text click or hover

--- libs/recipe-ui/src/lib/frame/frame.stories.ts
+++ libs/recipe-ui/src/lib/frame/frame.stories.ts
@@ -19,13 +19,63 @@ export const overflow = () =>
         blocks: [
           {
             type: BlockType.Text,
-            text:
-              "Before setting up request validation, we can see that:\n- the farm detail `GET /farms/{farmId}` route is accessible even though it's not defined in the OpenAPI Specification,\n- we can send data in any format and not only `application/json` as described in the OpenAPI Specification,\n- we can send any data when creating a farm using the `POST /farms` route.",
+            text: `Before setting up request validation, we can see that:
+- the farm detail \`GET /farms/{farmId}\` route is accessible even though it's not defined in the OpenAPI Specification,
+- we can send data in any format and not only \`application/json\` as described in the OpenAPI Specification,
+- we can send any data when creating a farm using the \`POST /farms\` route.`,
           },
           {
             type: BlockType.Code,
-            code:
-              '# Get a farm.\ncurl http://localhost:8080/farms/P4VU2Xsw\n\n# Create a farm with urlencoded data\ncurl http://localhost:8080/farms -d"name=springfield"\n\n# Create a farm with invalid data.\ncurl http://localhost:8080/farms \\\n  -H "Content-Type: application/json" \\\n  -d \'{"name": 123, "random": "data"}\'\n\nblalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla ',
+            code: `# Get a farm.
+curl http://localhost:8080/farms/P4VU2Xsw
+
+# Create a farm with urlencoded data
+curl http://localhost:8080/farms -d"name=springfield"
+
+# Create a farm with invalid data.
+curl http://localhost:8080/farms
+    -H "Content-Type: application/json"
+    -d \'{"name": 123, "random": "data"}\'
+    blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla blalabla`,
+            language: 'shell',
+          },
+        ],
+        duration: 2,
+        slug: 'without-validation',
+        title: 'Without validation',
+      }),
+    },
+  } as Meta);
+
+export const highlight = () =>
+  ({
+    title: 'Highlight',
+    moduleMetadata: {
+      imports: [FrameModule],
+    },
+    component: FrameComponent,
+    props: {
+      frame: createFrame({
+        blocks: [
+          {
+            type: BlockType.Text,
+            text: `Before setting up request validation, we can see that:
+- the farm detail \`GET /farms/{farmId}\` route is accessible even though it's not defined in the OpenAPI Specification,
+- we can send data in any format and not only \`application/json\` as described in the OpenAPI Specification,
+- we can send any data when creating a farm using the \`POST /farms\` route.`,
+          },
+          {
+            type: BlockType.Code,
+            code: `# Get a farm.
+curl http://localhost:8080/farms/P4VU2Xsw
+
+# Create a farm with urlencoded data
+curl http://localhost:8080/farms -d"name=springfield"
+
+# Create a farm with invalid data.
+curl http://localhost:8080/farms
+    -H "Content-Type: application/json"
+    -d \'{"name": 123, "random": "data"}\'`,
             language: 'shell',
           },
         ],
