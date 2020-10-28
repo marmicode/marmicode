40a17b0bc56dbe8fc59f6b9f53137b769e186596
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-28 17:52:15 +0100

    🚧 highlight lines on text click or hover

--- libs/recipe-ui/src/lib/frame/frame.stories.ts
+++ libs/recipe-ui/src/lib/frame/frame.stories.ts
@@ -60,9 +60,9 @@ export const highlight = () =>
           {
             type: BlockType.Text,
             text: `Before setting up request validation, we can see that:
-- the farm detail \`GET /farms/{farmId}\` route is accessible even though it's not defined in the OpenAPI Specification,
-- we can send data in any format and not only \`application/json\` as described in the OpenAPI Specification,
-- we can send any data when creating a farm using the \`POST /farms\` route.`,
+- [the farm detail \`GET /farms/{farmId}\` route is accessible even though it's not defined in the OpenAPI Specification](lines://2),
+- [we can send data in any format and not only \`application/json\` as described in the OpenAPI Specification](lines://5),
+- [we can send any data when creating a farm using the \`POST /farms\` route.](lines://8-10)`,
           },
           {
             type: BlockType.Code,
