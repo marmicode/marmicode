b9e54689782c3741be87f50475d69ae145dd9921
Younes Jaaidi <yjaaidi@gmail.com>
2020-10-30 16:40:56 +0100

    🚧 reset highlight on frame change

---
+++ libs/recipe-ui/src/lib/frame/frame.component.spec.ts
@@ -0,0 +1,58 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { BlockType, createFrame } from '@marmicode/recipe-core';
+import { first } from 'rxjs/operators';
+import { createHighlightZone } from '../highlight/highlight-zone';
+import { FrameComponent } from './frame.component';
+
+describe('FrameComponent', () => {
+  let component: FrameComponent;
+  let fixture: ComponentFixture<FrameComponent>;
+  const frame = createFrame({
+    blocks: [],
+    duration: 2,
+    slug: 'without-validation',
+    title: 'Without validation',
+  });
+
+  beforeEach(async () => {
+    return TestBed.configureTestingModule({
+      declarations: [FrameComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FrameComponent);
+    component = fixture.componentInstance;
+    component.frame = frame;
+    fixture.detectChanges();
+  });
+
+  it('should not reset highlight zone if same frame reference is set', async () => {
+    component.onHighlightZone(
+      createHighlightZone({
+        color: 'red',
+        sections: [],
+      })
+    );
+    component.frame = frame;
+    expect(await component.highlightZone$.pipe(first()).toPromise()).not.toBe(
+      null
+    );
+  });
+
+  xit('should reset highlight zone on frame change', async () => {
+    component.onHighlightZone(
+      createHighlightZone({
+        color: 'red',
+        sections: [],
+      })
+    );
+    component.frame = createFrame({
+      blocks: [],
+      duration: 3,
+      slug: 'another-one',
+      title: 'Another One',
+    });
+    expect(await component.highlightZone$.pipe(first()).toPromise()).toBe(null);
+  });
+});
