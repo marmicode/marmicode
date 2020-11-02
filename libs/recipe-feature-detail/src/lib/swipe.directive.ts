81343fa7164514a5b14c5a815f28f5abc6b10445
Younes Jaaidi <yjaaidi@gmail.com>
2020-11-02 19:44:52 +0100

    🚧 swipe through frames: add *mcSwipe directive

--- libs/recipe-feature-detail/src/lib/swipe.directive.ts
+++ libs/recipe-feature-detail/src/lib/swipe.directive.ts
@@ -1,10 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Directive, NgModule } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  NgModule,
+  OnInit,
+} from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Subject } from 'rxjs';
 
+@UntilDestroy()
 @Directive({
   selector: '[mcSwipe]',
 })
-export class SwipeDirective {}
+export class SwipeDirective implements OnInit {
+  private _touchstart$ = new Subject();
+
+  constructor(private _elementRef: ElementRef) {}
+
+  ngOnInit() {}
+
+  @HostListener('touchstart', ['$event']) onTouchStart(evt: MouseEvent) {
+    this._touchstart$.next(evt);
+  }
+}
 
 @NgModule({
   declarations: [SwipeDirective],
