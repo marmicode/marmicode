a2ed5bf343024a68ade3d37eb096fabf9796d115
Younes Jaaidi <yjaaidi@gmail.com>
2020-12-22 14:39:42 +0100

    🧹 tidy up

--- libs/resource-feature-search/src/index.ts
+++ libs/resource-feature-search/src/index.ts
@@ -1,6 +1,2 @@
 export { ResourceSearchRoutingModule } from './lib/resource-search-routing.module';
-export {
-  ResourceSearchFormComponent,
-  ResourceSearchFormModule,
-} from './lib/resource-search-form.component';
 export { resourceSearchRouterHelper } from '@marmicode/shared-router-helpers';
