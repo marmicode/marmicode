d53033ef77598cdd99085937bed72e4ee1342c6c
Younes Jaaidi <yjaaidi@gmail.com>
2020-12-22 17:34:24 +0100

    🧹 remove useless option

--- apps/marmicode/src/app/app-routing.module.ts
+++ apps/marmicode/src/app/app-routing.module.ts
@@ -48,7 +48,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
