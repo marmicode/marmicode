72c16de52b96c1b5563b335f76fb191a42119c1b
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-06 18:19:43 +0100

    🚧 display blog post: convert text to frames

---
+++ libs/blog-post-ui/src/lib/text-to-frames.spec.ts
@@ -0,0 +1,11 @@
+export function textToFrames(text: string) {
+  throw new Error('🚧 work in progress!');
+}
+
+describe('textToFrames', () => {
+  xit(`🚧 should convert blog post's text to frames`, () => {
+    const frames = textToFrames(``);
+
+    expect(frames).toEqual([]);
+  });
+});
