ae304bc5f0a3b457158ecf267252b70687c155f8
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-06 16:48:56 +0100

    🧹 tidy up

--- libs/blog-post-ui/src/lib/blog-post.ts
+++ libs/blog-post-ui/src/lib/blog-post.ts
@@ -1,16 +1,13 @@
-import { ResourceType } from '@marmicode/resource-core';
-
 export interface BlogPostContent {
   text: string;
 }
 
 export interface BlogPost {
   id: string;
-  type: ResourceType.BlogPost;
   title: string;
   content: BlogPostContent;
 }
 
-export function createBlogPost(blogPost: Omit<BlogPost, 'type'>) {
-  return { ...blogPost, type: ResourceType.BlogPost };
+export function createBlogPost(blogPost: BlogPost) {
+  return { ...blogPost };
 }
