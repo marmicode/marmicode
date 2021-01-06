b763bd59be97536ed03b5c30786794914f9d1d00
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-06 19:05:28 +0100

    🚧 display blog post: convert text to frames

--- libs/blog-post-ui/src/lib/markdown-to-frames.spec.ts
+++ libs/blog-post-ui/src/lib/markdown-to-frames.spec.ts
@@ -1,10 +1,10 @@
-import { Block, createCodeBlock, createTextBlock } from '@marmicode/frame-api';
+import { createCodeBlock, createTextBlock, Frame } from '@marmicode/frame-api';
 
-export function markdownToBlocks(text: string): Block[] {
+export function markdownToFrames(text: string): Frame[] {
   throw new Error('🚧 work in progress!');
 }
 
-describe('markdownToBlocks', () => {
+describe('markdownToFrames', () => {
   xit(`🚧 should convert blog post's text to blocks`, () => {
     const markdown = `
 Intro.
@@ -32,43 +32,65 @@ code C
 Chapter C after code.
 `;
 
-    const blocks = markdownToBlocks(markdown);
+    const frames = markdownToFrames(markdown);
 
-    expect(blocks.length).toEqual(5);
-    expect(blocks).toEqual([
-      createTextBlock({
-        text: `
+    expect(frames.length).toEqual(4);
+    expect(frames).toEqual([
+      expect.objectContaining({
+        blocks: [
+          createTextBlock({
+            text: `
 Intro.
-
+      `,
+          }),
+        ],
+      } as Partial<Frame>),
+      expect.objectContaining({
+        blocks: [
+          createTextBlock({
+            text: `
 # Title A
 
 Chapter A.
       `,
-      }),
-      createCodeBlock({
-        language: 'javascript',
-        code: `code A`,
-      }),
-      createTextBlock({
-        text: `
+          }),
+          createCodeBlock({
+            language: 'javascript',
+            code: `code A`,
+          }),
+        ],
+      } as Partial<Frame>),
+      expect.objectContaining({
+        blocks: [
+          createTextBlock({
+            text: `
 # Title B
 
 Chapter B.
-
+      `,
+          }),
+        ],
+      } as Partial<Frame>),
+      expect.objectContaining({
+        blocks: [
+          createTextBlock({
+            text: `
 # Title C
 
 Chapter C before code.
         `,
-      }),
-      createCodeBlock({
-        language: 'javascript',
-        code: `code C`,
-      }),
-      createTextBlock({
-        text: `
+          }),
+          createCodeBlock({
+            language: 'javascript',
+            code: `code C`,
+          }),
+          createTextBlock({
+            text: `
 Chapter C after code.
 `,
-      }),
+          }),
+        ],
+      } as Partial<Frame>),
     ]);
   });
 });
