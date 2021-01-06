7c9b7acfeab3c8a7747b2c97f70f81f7032f8b65
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-06 18:45:02 +0100

    🚧 display blog post: convert text to frames

--- libs/blog-post-ui/src/lib/markdown-to-frames.spec.ts
+++ libs/blog-post-ui/src/lib/markdown-to-frames.spec.ts
@@ -1,11 +1,74 @@
-export function markdownToFrames(text: string) {
+import { Block, createCodeBlock, createTextBlock } from '@marmicode/frame-api';
+
+export function markdownToBlocks(text: string): Block[] {
   throw new Error('🚧 work in progress!');
 }
 
-describe('markdownToFrames', () => {
-  xit(`🚧 should convert blog post's text to frames`, () => {
-    const frames = markdownToFrames(``);
+describe('markdownToBlocks', () => {
+  xit(`🚧 should convert blog post's text to blocks`, () => {
+    const markdown = `
+Intro.
 
-    expect(frames).toEqual([]);
+# Title A
+
+Chapter A.
+
+\`\`\`javascript
+code A
+\`\`\`
+
+# Title B
+
+Chapter B.
+
+# Title C
+
+Chapter C before code.
+
+\`\`\`javascript
+code C
+\`\`\`
+
+Chapter C after code.
+`;
+
+    const blocks = markdownToBlocks(markdown);
+
+    expect(blocks.length).toEqual(5);
+    expect(blocks).toEqual([
+      createTextBlock({
+        text: `
+Intro.
+
+# Title A
+
+Chapter A.
+      `,
+      }),
+      createCodeBlock({
+        language: 'javascript',
+        code: `code A`,
+      }),
+      createTextBlock({
+        text: `
+# Title B
+
+Chapter B.
+
+# Title C
+
+Chapter C before code.
+        `,
+      }),
+      createCodeBlock({
+        language: 'javascript',
+        code: `code C`,
+      }),
+      createTextBlock({
+        text: `
+Chapter C after code.
+`,
+      }),
+    ]);
   });
 });
