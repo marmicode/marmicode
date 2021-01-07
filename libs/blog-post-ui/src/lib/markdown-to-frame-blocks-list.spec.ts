ab975ce241d03b5afc72397907ebdb976a6f4d8f
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-07 20:43:25 +0100

    🚧 display blog post: convert markdown to block lists

--- libs/blog-post-ui/src/lib/markdown-to-frame-blocks-list.spec.ts
+++ libs/blog-post-ui/src/lib/markdown-to-frame-blocks-list.spec.ts
@@ -9,17 +9,24 @@ export interface BlockGroup {
   blocks: Block[];
 }
 
+export function createBlockGroup(blockGroup: BlockGroup): BlockGroup {
+  return { ...blockGroup };
+}
+
 export function markdownToFrameBlockGroups(text: string): BlockGroup[] {
   const tokens = parseMarkdown(text);
   return tokens.reduce((blockGroups) => {
-    return blockGroups;
+    /* Append to last block group. */
+    const lastBlockGroup =
+      blockGroups[blockGroups.length - 1] ?? createBlockGroup({ blocks: [] });
+    return [...blockGroups.slice(0, blockGroups.length - 1), lastBlockGroup];
   }, [] as BlockGroup[]);
 }
 
 export const like = expect.objectContaining;
 
 describe('markdownToFrames', () => {
-  xit(`🚧 should convert blog post's text to blocks`, () => {
+  it(`🚧 should convert blog post's text to blocks`, () => {
     const markdown = `
 Intro.
 
@@ -48,83 +55,83 @@ Chapter C after code.
 
     const frameBlocksList = markdownToFrameBlockGroups(markdown);
 
-    expect(frameBlocksList.length).toEqual(4);
-    expect(frameBlocksList).toEqual([
-      /* Intro frame.*/
-      like({
-        blocks: [
-          like({
-            tokens: [
-              like({
-                type: 'paragraph',
-              }),
-            ],
-          } as Partial<MarkdownBlock>),
-        ],
-      }),
-
-      /* Chapter A. */
-      like({
-        blocks: [
-          like({
-            tokens: [
-              like({
-                type: 'header',
-              }),
-              like({
-                type: 'paragraph',
-              }),
-            ],
-          } as Partial<MarkdownBlock>),
-          like({
-            language: 'javascript',
-            code: 'Code A',
-          } as Partial<CodeBlock>),
-        ],
-      }),
-
-      /* Chapter B. */
-      like({
-        blocks: [
-          like({
-            tokens: [
-              like({
-                type: 'header',
-              }),
-              like({
-                type: 'paragraph',
-              }),
-            ],
-          } as Partial<MarkdownBlock>),
-        ],
-      }),
-
-      /* Chapter C. */
-      like({
-        blocks: [
-          like({
-            tokens: [
-              like({
-                type: 'header',
-              }),
-              like({
-                type: 'paragraph',
-              }),
-            ],
-          } as Partial<MarkdownBlock>),
-          like({
-            language: 'javascript',
-            code: 'Code C',
-          } as Partial<CodeBlock>),
-          like({
-            tokens: [
-              like({
-                type: 'paragraph',
-              }),
-            ],
-          } as Partial<MarkdownBlock>),
-        ],
-      }),
-    ]);
+    // expect(frameBlocksList.length).toEqual(4);
+    // expect(frameBlocksList).toEqual([
+    //   /* Intro frame.*/
+    //   like({
+    //     blocks: [
+    //       like({
+    //         tokens: [
+    //           like({
+    //             type: 'paragraph',
+    //           }),
+    //         ],
+    //       } as Partial<MarkdownBlock>),
+    //     ],
+    //   }),
+    //
+    //   /* Chapter A. */
+    //   like({
+    //     blocks: [
+    //       like({
+    //         tokens: [
+    //           like({
+    //             type: 'header',
+    //           }),
+    //           like({
+    //             type: 'paragraph',
+    //           }),
+    //         ],
+    //       } as Partial<MarkdownBlock>),
+    //       like({
+    //         language: 'javascript',
+    //         code: 'Code A',
+    //       } as Partial<CodeBlock>),
+    //     ],
+    //   }),
+    //
+    //   /* Chapter B. */
+    //   like({
+    //     blocks: [
+    //       like({
+    //         tokens: [
+    //           like({
+    //             type: 'header',
+    //           }),
+    //           like({
+    //             type: 'paragraph',
+    //           }),
+    //         ],
+    //       } as Partial<MarkdownBlock>),
+    //     ],
+    //   }),
+    //
+    //   /* Chapter C. */
+    //   like({
+    //     blocks: [
+    //       like({
+    //         tokens: [
+    //           like({
+    //             type: 'header',
+    //           }),
+    //           like({
+    //             type: 'paragraph',
+    //           }),
+    //         ],
+    //       } as Partial<MarkdownBlock>),
+    //       like({
+    //         language: 'javascript',
+    //         code: 'Code C',
+    //       } as Partial<CodeBlock>),
+    //       like({
+    //         tokens: [
+    //           like({
+    //             type: 'paragraph',
+    //           }),
+    //         ],
+    //       } as Partial<MarkdownBlock>),
+    //     ],
+    //   }),
+    // ]);
   });
 });
