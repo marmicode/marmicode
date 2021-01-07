5b3b41b2d1f54adb2a9d2622bba6dbeee91fdd75
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-07 14:07:43 +0100

    🚧 display blog post: render highlight link

--- libs/frame-ui/src/lib/markdown-block/markdown-token-link.component.ts
+++ libs/frame-ui/src/lib/markdown-block/markdown-token-link.component.ts
@@ -5,35 +5,55 @@ import {
   OnChanges,
   SimpleChanges,
 } from '@angular/core';
+import { select } from '@ngrx/store';
+import { RxState } from '@rx-angular/state';
 import { Tokens } from 'marked';
+import { combineLatest } from 'rxjs';
 import { HighlightLinkComponent } from '../highlight/highlight-link.component';
+import { MarkdownBlockStateService } from './markdown-block-state.service';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-markdown-token-link',
   template: ` <mc-highlight-link
-      *ngIf="isHighlightLink"
-      [color]="color"
-      [href]="token.href"
-      ><mc-markdown-tokens [tokens]="token.tokens"></mc-markdown-tokens
+      *ngIf="isHighlightLink$ | async"
+      [color]="color$ | async"
+      [href]="href$ | async"
+      ><mc-markdown-tokens [tokens]="tokens$ | async"></mc-markdown-tokens
     ></mc-highlight-link>
-    <a *ngIf="!isHighlightLink" [href]="token.href" target="_blank">
-      <mc-markdown-tokens [tokens]="token.tokens"></mc-markdown-tokens>
+    <a
+      *ngIf="(isHighlightLink$ | async) === false"
+      [href]="href$ | async"
+      target="_blank"
+    >
+      <mc-markdown-tokens [tokens]="tokens$ | async"></mc-markdown-tokens>
     </a>`,
 })
-export class MarkdownTokenLinkComponent implements OnChanges {
-  @Input() token: Tokens.Link;
-  color: string;
-  isHighlightLink: boolean;
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.token) {
-      const href = this.token.href;
-      this.color = HighlightLinkComponent.getColor({
-        highlightableZones: [],
-        href,
-      });
-      this.isHighlightLink = HighlightLinkComponent.canHandleLink(href);
-    }
+export class MarkdownTokenLinkComponent {
+  @Input() set token(token: Tokens.Link) {
+    this._state.set({ token });
   }
+
+  href$ = this._state.select('token', 'href');
+  tokens$ = this._state.select('token', 'tokens');
+
+  color$ = combineLatest([
+    this._markdownBlockStateService.highlightableZones$,
+    this.href$,
+  ]).pipe(
+    select(([highlightableZones, href]) =>
+      HighlightLinkComponent.getColor({
+        highlightableZones,
+        href,
+      })
+    )
+  );
+  isHighlightLink$ = this.href$.pipe(
+    select((href) => HighlightLinkComponent.canHandleLink(href))
+  );
+
+  constructor(
+    private _markdownBlockStateService: MarkdownBlockStateService,
+    private _state: RxState<{ token: Tokens.Link }>
+  ) {}
 }
