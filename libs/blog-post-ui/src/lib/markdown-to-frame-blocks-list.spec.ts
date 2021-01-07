5a652aacd8557a8bfaa795b628cb73ef78d80666
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-07 20:38:03 +0100

    🚧 display blog post: convert markdown to block lists

--- libs/blog-post-ui/src/lib/markdown-to-frame-blocks-list.spec.ts
+++ libs/blog-post-ui/src/lib/markdown-to-frame-blocks-list.spec.ts
@@ -5,11 +5,15 @@ import {
   parseMarkdown,
 } from '@marmicode/frame-api';
 
-export function markdownToFrameBlocksList(text: string): Block[][] {
+export interface BlockGroup {
+  blocks: Block[];
+}
+
+export function markdownToFrameBlockGroups(text: string): BlockGroup[] {
   const tokens = parseMarkdown(text);
-  return tokens.reduce((blocks) => {
-    return blocks;
-  }, [] as Block[][]);
+  return tokens.reduce((blockGroups) => {
+    return blockGroups;
+  }, [] as BlockGroup[]);
 }
 
 export const like = expect.objectContaining;
@@ -42,77 +46,85 @@ code C
 Chapter C after code.
 `;
 
-    const frameBlocksList = markdownToFrameBlocksList(markdown);
+    const frameBlocksList = markdownToFrameBlockGroups(markdown);
 
     expect(frameBlocksList.length).toEqual(4);
     expect(frameBlocksList).toEqual([
       /* Intro frame.*/
-      like([
-        like({
-          tokens: [
-            like({
-              type: 'paragraph',
-            }),
-          ],
-        } as Partial<MarkdownBlock>),
-      ]),
+      like({
+        blocks: [
+          like({
+            tokens: [
+              like({
+                type: 'paragraph',
+              }),
+            ],
+          } as Partial<MarkdownBlock>),
+        ],
+      }),
 
       /* Chapter A. */
-      like([
-        like({
-          tokens: [
-            like({
-              type: 'header',
-            }),
-            like({
-              type: 'paragraph',
-            }),
-          ],
-        } as Partial<MarkdownBlock>),
-        like({
-          language: 'javascript',
-          code: 'Code A',
-        } as Partial<CodeBlock>),
-      ]),
+      like({
+        blocks: [
+          like({
+            tokens: [
+              like({
+                type: 'header',
+              }),
+              like({
+                type: 'paragraph',
+              }),
+            ],
+          } as Partial<MarkdownBlock>),
+          like({
+            language: 'javascript',
+            code: 'Code A',
+          } as Partial<CodeBlock>),
+        ],
+      }),
 
       /* Chapter B. */
-      like([
-        like({
-          tokens: [
-            like({
-              type: 'header',
-            }),
-            like({
-              type: 'paragraph',
-            }),
-          ],
-        } as Partial<MarkdownBlock>),
-      ]),
+      like({
+        blocks: [
+          like({
+            tokens: [
+              like({
+                type: 'header',
+              }),
+              like({
+                type: 'paragraph',
+              }),
+            ],
+          } as Partial<MarkdownBlock>),
+        ],
+      }),
 
       /* Chapter C. */
-      like([
-        like({
-          tokens: [
-            like({
-              type: 'header',
-            }),
-            like({
-              type: 'paragraph',
-            }),
-          ],
-        } as Partial<MarkdownBlock>),
-        like({
-          language: 'javascript',
-          code: 'Code C',
-        } as Partial<CodeBlock>),
-        like({
-          tokens: [
-            like({
-              type: 'paragraph',
-            }),
-          ],
-        } as Partial<MarkdownBlock>),
-      ]),
+      like({
+        blocks: [
+          like({
+            tokens: [
+              like({
+                type: 'header',
+              }),
+              like({
+                type: 'paragraph',
+              }),
+            ],
+          } as Partial<MarkdownBlock>),
+          like({
+            language: 'javascript',
+            code: 'Code C',
+          } as Partial<CodeBlock>),
+          like({
+            tokens: [
+              like({
+                type: 'paragraph',
+              }),
+            ],
+          } as Partial<MarkdownBlock>),
+        ],
+      }),
     ]);
   });
 });
