fa77595364b2361e4a493343548b804454dd5886
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-07 10:56:58 +0100

    🚧 display blog post: <mc-markdown-block>

--- libs/frame-ui/src/lib/markdown-block/markdown-block.component.ts
+++ libs/frame-ui/src/lib/markdown-block/markdown-block.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  NgModule,
+} from '@angular/core';
 import { MarkdownBlock } from '@marmicode/frame-core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-markdown-block',
-  template: `🚧 markdown-block`,
+  template: `
+    <ng-container *ngFor="let token of block.tokens" [ngSwitch]="token">
+    </ng-container>
+  `,
 })
 export class MarkdownBlockComponent {
-  block: MarkdownBlock;
+  @Input() block: MarkdownBlock;
 }
 
 @NgModule({
@@ -17,3 +25,27 @@ export class MarkdownBlockComponent {
   imports: [CommonModule],
 })
 export class MarkdownBlockModule {}
+
+// Tokens.Space
+// | Tokens.Code
+// | Tokens.Heading
+// | Tokens.Table
+// | Tokens.Hr
+// | Tokens.Blockquote
+// | Tokens.BlockquoteStart
+// | Tokens.BlockquoteEnd
+// | Tokens.List
+// | Tokens.ListItem
+// | Tokens.Paragraph
+// | Tokens.HTML
+// | Tokens.Text
+// | Tokens.Def
+// | Tokens.Escape
+// | Tokens.Tag
+// | Tokens.Image
+// | Tokens.Link
+// | Tokens.Strong
+// | Tokens.Em
+// | Tokens.Codespan
+// | Tokens.Br
+// | Tokens.Del;
