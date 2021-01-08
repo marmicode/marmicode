57fcc2fef181961712935bf22feb0fedbb523ced
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-08 12:02:33 +0100

    🚧 display blog post: render blog post's markdown text
    [#175767650]

--- libs/block-ui/src/lib/block.component.ts
+++ libs/block-ui/src/lib/block.component.ts
@@ -10,18 +10,30 @@ import {
 import { Block, BlockType } from '@marmicode/block-core';
 import { CodeBlockModule } from './code-block/code-block.component';
 import { HighlightZone } from './highlight/highlight-zone';
+import { MarkdownBlockModule } from './markdown-block/markdown-block.component';
 import { TextBlockModule } from './text-block.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-block',
   template: ` <ng-container [ngSwitch]="block.type">
+    <!-- Code. -->
     <mc-code-block
       *ngSwitchCase="BlockType.Code"
       [block]="block"
       [highlightableZones]="highlightableZones"
       [highlightZone]="highlightZone"
     ></mc-code-block>
+
+    <!-- Markdown. -->
+    <mc-markdown-block
+      *ngSwitchCase="BlockType.Markdown"
+      [block]="block"
+      [highlightableZones]="highlightableZones"
+      (highlightZoneChange)="highlightZoneChange.emit($event)"
+    ></mc-markdown-block>
+
+    <!-- Text (converts text to markdown tokens and uses <mc-markdown-block>). -->
     <mc-text-block
       *ngSwitchCase="BlockType.Text"
       [block]="block"
@@ -42,6 +54,11 @@ export class BlockComponent {
 @NgModule({
   declarations: [BlockComponent],
   exports: [BlockComponent],
-  imports: [CommonModule, TextBlockModule, CodeBlockModule],
+  imports: [
+    CommonModule,
+    TextBlockModule,
+    CodeBlockModule,
+    MarkdownBlockModule,
+  ],
 })
 export class BlockModule {}
