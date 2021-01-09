ea24b00d372a85f4b91710ffd53a9fa14500c3da
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-09 16:45:40 +0100

    🚧 build github workflows in order to handle anchors

--- tools/build-github-workflows/src/lib/build-github-workflows.spec.ts
+++ tools/build-github-workflows/src/lib/build-github-workflows.spec.ts
@@ -1,7 +1,54 @@
-import { buildGithubWorkflows } from './build-github-workflows';
+import { readdir } from 'fs';
+import * as fs from 'fs';
+import { callbackify } from 'util';
+import { buildGithubWorkflows, main } from './build-github-workflows';
+
+jest.mock('fs');
 
 describe('buildGithubWorkflows', () => {
-  it('should work', () => {
-    expect(buildGithubWorkflows()).toEqual('build-github-workflows');
+  beforeEach(() => {
+    jest.spyOn(fs, 'readdir').mockImplementation(
+      /* eslint-disable @typescript-eslint/no-explicit-any */
+      callbackify(jest.fn().mockResolvedValue(['main.yml'])) as any
+    );
+
+    jest.spyOn(fs, 'readFile').mockImplementation(
+      /* eslint-disable @typescript-eslint/no-explicit-any */
+      callbackify(
+        jest.fn().mockResolvedValue(`
+name: Test
+
+on:
+  - push
+  
+x-anchors:
+  setup: &setup
+    name: Setup
+    run: yarn install
+
+jobs:
+
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - *setup
+      - name: Test
+        run: yarn test
+
+
+ `)
+      ) as any
+    );
+  });
+
+  afterEach(() => {
+    (fs.readdir as jest.MockedFunction<any>).mockRestore();
+  });
+
+  xit('🚧 should convert anchors', async () => {
+    await buildGithubWorkflows();
+    // check fs.readdir call
+    // check fs.readFile call
+    // check fs.writeFile call
   });
 });
