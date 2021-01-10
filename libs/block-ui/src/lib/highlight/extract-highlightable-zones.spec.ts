fe1968ceba40fb06e371d74a88caaf8bd87ce36e
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-10 02:54:23 +0100

    🚧 display blog post: extract links from markdown block

--- libs/block-ui/src/lib/highlight/extract-highlightable-zones.spec.ts
+++ libs/block-ui/src/lib/highlight/extract-highlightable-zones.spec.ts
@@ -1,7 +1,7 @@
 import {
   BlockType,
   createBlockGroup,
-  createTextBlock,
+  parseMarkdown,
 } from '@marmicode/block-core';
 import { extractHighlightableZones } from './extract-highlightable-zones';
 
@@ -69,14 +69,78 @@ curl http://localhost:8080/farms
     ]);
   });
 
+  xit('🚧 should extract highlight info from markdown block', () => {
+    const frame = createBlockGroup({
+      blocks: [
+        {
+          type: BlockType.Markdown,
+          tokens: parseMarkdown(`Before setting up request validation, we can see that:
+- [the farm detail \`GET /farms/{farmId}\` route is accessible](highlight://2) even though it's not defined in the OpenAPI Specification,
+- [we can send data in any format](highlight://5) and not only \`application/json\` as described in the OpenAPI Specification,
+- [we can send any data](highlight://5,8-10) when creating a farm using the \`POST /farms\` route.
+- This is [another link highlighting the first zone](highlight://2)`),
+        },
+        {
+          type: BlockType.Code,
+          code: `# Get a farm.
+curl http://localhost:8080/farms/P4VU2Xsw
+
+# Create a farm with urlencoded data
+curl http://localhost:8080/farms -d"name=springfield"
+
+# Create a farm with invalid data.
+curl http://localhost:8080/farms
+    -H "Content-Type: application/json"
+    -d \'{"name": 123, "random": "data"}\'`,
+          language: 'shell',
+        },
+      ],
+    });
+
+    expect(extractHighlightableZones(frame)).toEqual([
+      {
+        color: 'blueviolet',
+        sections: [
+          {
+            start: 2,
+            end: 2,
+          },
+        ],
+      },
+      {
+        color: 'green',
+        sections: [
+          {
+            start: 5,
+            end: 5,
+          },
+        ],
+      },
+      {
+        color: 'darkcyan',
+        sections: [
+          {
+            start: 5,
+            end: 5,
+          },
+          {
+            start: 8,
+            end: 10,
+          },
+        ],
+      },
+    ]);
+  });
+
   it('should not fail if no link', () => {
     expect(
       extractHighlightableZones(
         createBlockGroup({
           blocks: [
-            createTextBlock({
-              text: 'test',
-            }),
+            {
+              type: BlockType.Markdown,
+              tokens: parseMarkdown('text'),
+            },
           ],
         })
       )
