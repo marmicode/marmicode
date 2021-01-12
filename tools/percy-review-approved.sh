a992d078786c6407a8481e2f9a360e2dccd49b0d
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-12 12:36:42 +0100

    🛠 check percy result in workflow

--- tools/percy-review-approved.sh
+++ tools/percy-review-approved.sh
@@ -6,7 +6,4 @@ set -e
 export PERCY_PROJECT=marmicode/marmicode
 COMMIT_HASH=$(git rev-parse HEAD)
 REVIEW_STATE=$(percy-poller --sha "$COMMIT_HASH" | npx json 'review-state')
-test "$REVIEW_STATE" == "approved"
-RESULT=$?
-
-exit $RESULT
+test "$REVIEW_STATE" = "approved"
