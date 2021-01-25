de4720f8f91245a498ffdeb40d1b0c5f6147f403
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-25 16:39:38 +0100

    🛠 wait for resources to appear

--- apps/marmicode-e2e/src/integration/app.spec.ts
+++ apps/marmicode-e2e/src/integration/app.spec.ts
@@ -1,8 +1,12 @@
 describe('marmicode', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
   it('should list resources', () => {
     cy.location('pathname').should('eq', '/learn/everything');
+    /* Wait for resources to appear. */
+    cy.get('mc-resource-card').its('length').should('eq', 11);
     cy.snapshot();
   });
 });
