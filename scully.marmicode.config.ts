530af3a4650cb318f7119875966ab204a31e9d2d
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-26 08:48:30 +0100

    ✅ prerender blog posts

--- scully.marmicode.config.ts
+++ scully.marmicode.config.ts
@@ -4,25 +4,46 @@ import axios from 'axios';
 export const config: ScullyConfig = {
   outDir: 'dist/apps/marmicode/static',
   routes: {},
-  extraRoutes: getExtraRoutes(),
+  extraRoutes: _getExtraRoutes(),
 };
 
-async function getExtraRoutes() {
-  return ['/services', ...(await getLearnBySkillRoutes())];
+async function _getExtraRoutes() {
+  return [
+    '/services',
+    ...(await _getLearnBySkillRoutes()),
+    ...(await _getBlogPostRoutes()),
+  ];
 }
 
-async function getLearnBySkillRoutes() {
-  const { data } = await axios.get(
-    'https://cdn.contentful.com/spaces/gowvxq3b4aid/environments/master/entries?content_type=skill&select=fields.slug',
-    {
-      headers: {
-        Authorization: `Bearer _d7Vb4r2lborp1uLTvsv0ne7fB0OMez2FscTnHQux5A`,
-      },
-    }
-  );
+async function _getBlogPostRoutes() {
+  const { data } = await _queryContentful({
+    content_type: 'resource',
+    'fields.resourceType': 'blog-post',
+    select: 'fields.slug',
+  });
+  return data.items.map((item) => `/blog/${item.fields.slug}`);
+}
+
+async function _getLearnBySkillRoutes() {
+  const { data } = await _queryContentful({
+    content_type: 'skill',
+    select: 'fields.slug',
+  });
   const skillSlugs = [
     'everything',
     ...data.items.map((item) => item.fields.slug),
   ];
   return skillSlugs.map((slug) => `/learn/${slug}`);
+}
+
+async function _queryContentful(params: { [key: string]: string }) {
+  return await axios.get(
+    'https://cdn.contentful.com/spaces/gowvxq3b4aid/environments/master/entries',
+    {
+      headers: {
+        Authorization: `Bearer _d7Vb4r2lborp1uLTvsv0ne7fB0OMez2FscTnHQux5A`,
+      },
+      params,
+    }
+  );
 }
