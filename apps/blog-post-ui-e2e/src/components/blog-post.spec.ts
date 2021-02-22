79250f63cff469ab6c9dbc76c20d7ac9595bb39b
Younes Jaaidi <yjaaidi@gmail.com>
2021-02-22 22:50:54 +0100

    🛠 use new mountStory's styles option

--- apps/blog-post-ui-e2e/src/components/blog-post.spec.ts
+++ apps/blog-post-ui-e2e/src/components/blog-post.spec.ts
@@ -6,30 +6,23 @@ import {
 } from '../../../../libs/blog-post-ui/src/lib/blog-post.stories';
 
 describe('BlogPostComponent', () => {
-  beforeEach(() => {
+  const styles = [
     /* @hack add missing style because this style is inserted dynamically
      * by fortawesome but after the destruction of the DOM for the second test,
      * the style is not inserted again due once CSS is mark as inserted.
      * (Cf. https://github.com/FortAwesome/Font-Awesome/blob/fcec2d1b01ff069ac10500ac42e4478d20d21f4c/js-packages/%40fortawesome/fontawesome-svg-core/index.js#L2164) */
-    /* @todo use `mountStory` style args. */
-    cy.document().then((doc) => {
-      const style: HTMLStyleElement = document.createElement('style');
-      const data = document.createTextNode(
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        require('!!raw-loader!../../../../node_modules/@fortawesome/fontawesome-svg-core/styles.css')
-          .default
-      );
-      style.appendChild(data);
-      doc.head.appendChild(style);
-    });
-  });
+    /* eslint-disable-next-line @typescript-eslint/no-var-requires */
+    require('!!raw-loader!../../../../node_modules/@fortawesome/fontawesome-svg-core/styles.css')
+      .default,
+  ];
+
   it('should show blog', () => {
-    mountStory(Default);
+    mountStory(Default, { styles });
     cy.snapshot();
   });
 
   it('should show video', () => {
-    mountStory(Video);
+    mountStory(Video, { styles });
     cy.snapshot();
   });
 });
