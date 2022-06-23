c1e114b16a298f60aaad974b5878e950e36b6319
Younes Jaaidi <yjaaidi@gmail.com>
2022-06-24 00:12:55 +0200

    test: ✅ fix cypress tests
    Cf. https://github.com/cypress-io/cypress/issues/19066
    & https://github.com/storybookjs/storybook/issues/14012

--- apps/cypress-plugins.js
+++ apps/cypress-plugins.js
@@ -1,6 +1,35 @@
-const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
-
+const webpackPreprocessor = require('@cypress/webpack-batteries-included-preprocessor');
 module.exports = (on, config) => {
-  on('file:preprocessor', preprocessTypescript(config));
+
+  /* Setup Percy. */
   on('task', require('@percy/cypress/task'));
+
+  /* @hack fix error. */
+  on('file:preprocessor', getWebpackPreprocessor());
+
+  return config;
 };
+
+function getWebpackPreprocessor() {
+  const webpackOptions = webpackPreprocessor.defaultOptions.webpackOptions;
+
+  webpackOptions.module.rules.unshift({
+    test: /[/\\]@angular[/\\].+\.m?js$/,
+    resolve: {
+      fullySpecified: false,
+    },
+    use: {
+      loader: 'babel-loader',
+      options: {
+        plugins: ['@angular/compiler-cli/linker/babel'],
+        compact: false,
+        cacheDirectory: true,
+      },
+    },
+  });
+
+  return webpackPreprocessor({
+    webpackOptions: webpackOptions,
+    typescript: require.resolve('typescript'),
+  });
+}
