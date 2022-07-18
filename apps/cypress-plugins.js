8d65b8b90cba7f2d04c6ece568d37b0e4f142c29
Younes Jaaidi <yjaaidi@gmail.com>
2022-07-18 23:17:34 +0200

    test: ✅ fix blog-post-ui component tests

--- apps/cypress-plugins.js
+++ apps/cypress-plugins.js
@@ -1,35 +1,6 @@
-const webpackPreprocessor = require('@cypress/webpack-batteries-included-preprocessor');
 module.exports = (on, config) => {
-
   /* Setup Percy. */
   on('task', require('@percy/cypress/task'));
 
-  /* @hack fix error. */
-  on('file:preprocessor', getWebpackPreprocessor());
-
   return config;
 };
-
-function getWebpackPreprocessor() {
-  const webpackOptions = webpackPreprocessor.defaultOptions.webpackOptions;
-
-  webpackOptions.module.rules.unshift({
-    test: /[/\\]@angular[/\\].+\.m?js$/,
-    resolve: {
-      fullySpecified: false,
-    },
-    use: {
-      loader: 'babel-loader',
-      options: {
-        plugins: ['@angular/compiler-cli/linker/babel'],
-        compact: false,
-        cacheDirectory: true,
-      },
-    },
-  });
-
-  return webpackPreprocessor({
-    webpackOptions: webpackOptions,
-    typescript: require.resolve('typescript'),
-  });
-}
