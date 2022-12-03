a904831ab802bca23efaa4f2ad77710c80cbc9b1
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-03 15:08:47 +0100

    refactor: 🛠 uprade to Cypress 11
    nx g @nrwl/cypress:migrate-to-cypress-10
    then fix cypress.config.ts by import @jscutlery/cypress-harness/preprocessor-config

---
+++ apps/marmicode-e2e/cypress.config.ts
@@ -0,0 +1,10 @@
+import { getPreprocessorConfig } from '@jscutlery/cypress-harness/preprocessor-config';
+import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    ...getPreprocessorConfig()
+  }
+});
