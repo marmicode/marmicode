889eb6cabc3ad1a21aee49a4d95ee416b65df8cc
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-04 16:10:25 +0100

    refactor: 🛠 fix lint issues

--- libs/testing/src/cypress-commands.ts
+++ libs/testing/src/cypress-commands.ts
@@ -2,6 +2,7 @@
 import '@percy/cypress';
 
 declare global {
+// eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable<Subject> {
       getByDataRole<E extends HTMLElement>(
