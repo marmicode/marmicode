81c6f56616bedffeebe0e75f3ae8520bc2667040
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-12 19:01:37 +0100

    Revert "test: ✅ revert to classic snapshots and disable ct & e2e caching"
    This reverts commit 7b2acf8a58e5552e3b95fad1b346c3180fabdf79.

---
+++ tools/upload-percy-snapshots.ts
@@ -0,0 +1,72 @@
+/* @hack fix "Could not find a declaration file for module"
+ * as `allowJs: true` was not enough. */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore-next-line
+import utils from '@percy/sdk-utils';
+import { readdir, readFile } from 'fs/promises';
+import { join } from 'path';
+import { argv } from 'process';
+
+async function main(projectPath?: string) {
+  if (projectPath == null) {
+    throw new Error(
+      'No project path provided. Usage: percy-upload-snapshots <project-path>'
+    );
+  }
+
+  console.info(`Uploading snapshots for ${projectPath}...`);
+
+  const files = await readdir(join(projectPath, '__percy_snapshots__'));
+
+  const snapshotInfoList: SnapshotInfo[] = await Promise.all(
+    files.map(async (fileName) => {
+      return JSON.parse(
+        await readFile(
+          join(projectPath, '__percy_snapshots__', fileName),
+          'utf8'
+        )
+      );
+    })
+  );
+
+  for (const snapshotInfo of snapshotInfoList) {
+    await _uploadSnapshot(snapshotInfo);
+  }
+}
+
+async function _uploadSnapshot(
+  snapshotInfo: SnapshotInfo,
+  remainingAttempts: number = ATTEMPTS - 1
+) {
+  console.info(
+    `Uploading snapshot ${snapshotInfo.name}... (${remainingAttempts} attempts left)`
+  );
+
+  try {
+    await utils.postSnapshot(snapshotInfo);
+  } catch (error) {
+    console.warn(error);
+
+    if (remainingAttempts === 0) {
+      return;
+    }
+
+    setTimeout(
+      () => _uploadSnapshot(snapshotInfo, remainingAttempts - 1),
+      (1000 * ATTEMPTS) / remainingAttempts
+    );
+  }
+}
+
+const ATTEMPTS = 5;
+
+interface SnapshotInfo {
+  name: string;
+  domSnapshot: {
+    html: string;
+  };
+  url: string;
+  widths: number[];
+}
+
+main(argv[2]);
