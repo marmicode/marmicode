6ae2fc40b5b10c8b15ad3dfff21e375c211d2f5d
Younes Jaaidi <yjaaidi@gmail.com>
2022-12-16 00:03:46 +0100

    test: ✅ inline styles & images in dom snapshots

--- libs/testing/src/cypress-commands.ts
+++ libs/testing/src/cypress-commands.ts
@@ -22,26 +22,82 @@ Cypress.Commands.add('getByDataRole', (dataRole) =>
   cy.get(`[data-role="${dataRole}"]`)
 );
 
-/* Cf. https://github.com/percy/percy-cypress/blob/master/index.js */
+/**
+ * The main purpose of this command is to capture the DOM snapshot
+ * in a file in order to upload it later to Percy.
+ *
+ * We had to manually inline styles and images.
+ * This is the only way we found to be able to cache Cypress test
+ * results with Nx but still be able to use Percy and upload all
+ * snapshots in the main branch.
+ *
+ * The main problem we met is that Percy's snapshot
+ * has a discovery phase which is taken care of by the local
+ * percy server (@percy/cli) which will automatically start
+ * uploading the build to Percy once the discovery phase is done.
+ *
+ * Cf. https://docs.percy.io/docs/debugging-sdks
+ */
 Cypress.Commands.add('snapshot', (name = undefined) => {
-  name = name || (cy as any).state('runnable').fullTitle();
+  name = name ?? Cypress.currentTest.titlePath.join(' ');
   const fileName = name.replace(/ /g, '-').toLowerCase();
 
   const options = {
     widths: [360, 768, 1280],
   };
 
-  return cy.document({ log: false }).then((dom) => {
-    const domSnapshot = serializeDOM({ ...options, dom });
-    return cy.writeFile(
+  return cy.document({ log: false }).then(async (document) => {
+    /* Inline styles. */
+    for (const el of Array.from(
+      document.querySelectorAll('link[rel="stylesheet"]')
+    )) {
+      const url = el.getAttribute('href');
+      if (!isSameOrigin({ url, document })) {
+        continue;
+      }
+
+      const res = await fetch(url);
+      const css = await res.text();
+      const style = document.createElement('style');
+      style.innerHTML = css;
+      el.parentNode?.insertBefore(style, el);
+      el.parentNode?.removeChild(el);
+    }
+
+    /* Inline images. */
+    for (const el of Array.from(document.querySelectorAll('img'))) {
+      const url = el.getAttribute('src');
+      if (!isSameOrigin({ url, document })) {
+        continue;
+      }
+
+      const res = await fetch(url);
+      const blob = await res.blob();
+      const reader = new FileReader();
+      reader.readAsDataURL(blob);
+      el.src = await new Promise((resolve) =>
+        reader.addEventListener('loadend', () =>
+          resolve(reader.result as string)
+        )
+      );
+    }
+
+    /* Serialize dom with Percy. */
+    const domSnapshot = serializeDOM({ ...options, dom: document });
+
+    cy.writeFile(
       `./__percy_snapshots__/${fileName}.json`,
       JSON.stringify({
         domSnapshot,
         name,
-        url: dom.URL,
+        url: document.URL,
         ...options,
       })
     );
   });
 });
 
+function isSameOrigin({ url, document }: { url: string; document: Document }) {
+  const origin = document.defaultView.origin;
+  return url.startsWith(origin) || url.match(/^\/[^/]/);
+}
