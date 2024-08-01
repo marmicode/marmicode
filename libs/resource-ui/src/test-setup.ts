6477701de15bddc0fe612296186d6dab841cbc83
Younes Jaaidi <younes@marmicode.io>
2024-08-02 01:21:09 +0200

    build: 📦 fix lint

--- libs/resource-ui/src/test-setup.ts
+++ libs/resource-ui/src/test-setup.ts
@@ -1,2 +1,2 @@
-import '../../../test-setup';
+import '@marmicode/shared-utils/test-setup';
 import 'jest-preset-angular/setup-jest';
--- libs/shared-utils/src/test-setup.ts
+++ libs/shared-utils/src/test-setup.ts
@@ -1,2 +1,7 @@
-import '../../../test-setup';
-import 'jest-preset-angular/setup-jest';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(globalThis as any)['ngJest'] = {
+  testEnvironmentOptions: {
+    errorOnUnknownElements: true,
+    errorOnUnknownProperties: true,
+  },
+};
