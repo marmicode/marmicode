bfa88a16be084e94a9c255b042215332629539a0
Younes Jaaidi <younes@marmicode.io>
2024-08-02 03:24:19 +0200

    build: 📦 fix prerender

--- apps/marmicode/src/main.ts
+++ apps/marmicode/src/main.ts
@@ -1,50 +1,7 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { enableProdMode, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { CONFIG as FIREBASE_ANALYTICS_CONFIG } from '@angular/fire/compat/analytics';
-import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { provideEffects } from '@ngrx/effects';
-import { provideRouterStore, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { provideStore } from '@ngrx/store';
-import { AppRoutingModule } from './app/app-routing.module';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideUpdateEffects, UpdateEffects } from './app/update/update.effects';
-import { environment } from './environments/environment';
+import { appConfig } from './app/app.config';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    ScreenTrackingService,
-    importProvidersFrom(AppRoutingModule),
-    importProvidersFrom(BrowserAnimationsModule),
-    importProvidersFrom(BrowserModule.withServerTransition({ appId: 'marmicode' })),
-    importProvidersFrom(ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production
-    })),
-    importProvidersFrom(StoreRouterConnectingModule.forRoot()),
-    provideAnalytics(() => getAnalytics()),
-    provideEffects(UpdateEffects),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideExperimentalZonelessChangeDetection(),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideRouterStore(),
-    provideStore(),
-    provideUpdateEffects(),
-    {
-      provide: FIREBASE_ANALYTICS_CONFIG,
-      useValue: {
-        allow_ad_personalization_signals: false,
-        allow_google_signals: false,
-        anonymize_ip: true
-      }
-    }
-  ]
-})
+bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-;
+
