75d91bb17b2b6031a427a7279bb15a2eb0a46173
Younes Jaaidi <younes@marmicode.io>
2025-03-25 03:10:03 +0100

    build: 📦 fix build

--- apps/marmicode/src/app/app.config.ts
+++ apps/marmicode/src/app/app.config.ts
@@ -1,12 +1,25 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+} from '@angular/fire/analytics';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { provideEffects } from '@ngrx/effects';
-import { provideRouterStore, StoreRouterConnectingModule } from '@ngrx/router-store';
+import {
+  provideRouterStore,
+  StoreRouterConnectingModule,
+} from '@ngrx/router-store';
 import { provideStore } from '@ngrx/store';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,10 +30,11 @@ export const appConfig: ApplicationConfig = {
     ScreenTrackingService,
     importProvidersFrom(AppRoutingModule),
     importProvidersFrom(BrowserAnimationsModule),
-    importProvidersFrom(BrowserModule.withServerTransition({ appId: 'marmicode' })),
-    importProvidersFrom(ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production
-    })),
+    importProvidersFrom(
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: environment.production,
+      }),
+    ),
     importProvidersFrom(StoreRouterConnectingModule.forRoot()),
     provideAnalytics(() => getAnalytics()),
     provideEffects(UpdateEffects),
@@ -29,6 +43,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideRouterStore(),
     provideStore(),
-    provideUpdateEffects()
-  ]
-}
+    provideUpdateEffects(),
+  ],
+};
