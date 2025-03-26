f51f6f5be2c379e4cc3fac5d106625cfb0fa78c9
Younes Jaaidi <younes@marmicode.io>
2025-03-26 09:57:10 +0100

    fix: 🐞 fix search (#177)

--- apps/marmicode-e2e/src/e2e/resource-search.cy.ts
+++ apps/marmicode-e2e/src/e2e/resource-search.cy.ts
@@ -3,42 +3,80 @@ import { getAllHarnesses } from '@jscutlery/cypress-harness';
 
 describe('resource search', () => {
   beforeEach(() => {
-    const responseMap = new Map([
-      [
-        'getAllResources',
+    cy.fixture('contentful/get-all-resources.json').then((allResources) => {
+      cy.intercept(
         {
-          fixture: 'contentful/get-all-resources.json',
+          path: '/content/v1/spaces/*/environments/master',
         },
-      ],
-      [
-        'getAllSkills',
-        {
-          fixture: 'contentful/get-all-skills.json',
+        (req) => {
+          switch (req.body.operationName) {
+            case 'getAllResources':
+              req.reply({
+                body: allResources,
+              });
+              break;
+            case 'getResourcesBySkill':
+              req.reply({
+                data: {
+                  skillCollection: {
+                    __typename: 'SkillCollection',
+                    items: [
+                      {
+                        __typename: 'Skill',
+                        linkedFrom: {
+                          __typename: 'SkillLinkingCollections',
+                          resourceCollection: {
+                            ...allResources.data.resourceCollection,
+                            items:
+                              allResources.data.resourceCollection.items.filter(
+                                (resource) =>
+                                  resource.skillCollection.items.some(
+                                    (skill) => {
+                                      console.log(
+                                        skill.slug ===
+                                          req.body.variables.skillSlug,
+                                      );
+                                      console.log(skill.slug);
+                                      console.log(req.body.variables.skillSlug);
+                                      return (
+                                        skill.slug ===
+                                        req.body.variables.skillSlug
+                                      );
+                                    },
+                                  ),
+                              ),
+                          },
+                        },
+                      },
+                    ],
+                  },
+                },
+              });
+              break;
+            case 'getAllSkills':
+              req.reply({
+                fixture: 'contentful/get-all-skills.json',
+              });
+              return;
+            default:
+              throw new Error(`Unexpected request.`);
+          }
         },
-      ],
-    ]);
+      );
+    });
 
-    cy.intercept(
-      {
-        path: '/content/v1/spaces/*/environments/master',
-      },
-      (req) => {
-        const response = responseMap.get(req.body.operationName);
-
-        if (response == null) {
-          throw new Error(`Unexpected request.`);
-        }
-
-        req.reply(response);
-      }
-    );
     cy.visit('/');
   });
 
   it('should list resources', () => {
     cy.location('pathname').should('eq', '/learn/everything');
     /* Wait for resources to appear. */
-    getAllHarnesses(ResourceCardHarness).should('have.length', 13);
+    getAllHarnesses(ResourceCardHarness).should('have.length', 14);
     cy.snapshot();
+  });
+
+  it('should filter resources by skill', () => {
+    cy.get('a').contains('Angular AOT Testing').click();
+    getAllHarnesses(ResourceCardHarness).should('have.length', 1);
   });
 });
