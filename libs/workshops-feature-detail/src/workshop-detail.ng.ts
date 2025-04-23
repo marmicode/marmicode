41fa55e37de75841814a5edb43887048893bc177
Younes Jaaidi <younes@marmicode.io>
2025-04-24 00:11:50 +0200

    refactor: 🛠️ add meta

--- libs/workshops-feature-detail/src/workshop-detail.ng.ts
+++ libs/workshops-feature-detail/src/workshop-detail.ng.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { PageComponent as Page } from '@marmicode/shared-ui';
+import { PageInfo } from 'libs/shared-ui/src/lib/page.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'mc-workshop-detail-page',
-  template: ``,
+  imports: [Page],
+  template: `<mc-page [info]="info()"> </mc-page>`,
 })
-export class WorkshopDetailPage {}
+export class WorkshopDetailPage {
+  info = signal<PageInfo>({
+    title: 'Pragmatic Angular Testing - Tapas Workshop',
+  });
+}
