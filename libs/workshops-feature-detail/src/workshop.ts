23dd70e42ee8043df0d41794f8672c5ad78944fc
Younes Jaaidi <younes@marmicode.io>
2025-06-23 23:51:44 +0200

    refactor: 🛠️ add Workshop type

---
+++ libs/workshops-feature-detail/src/workshop.ts
@@ -0,0 +1,46 @@
+export interface Workshop {
+  title: string;
+  type: 'tapas' | 'full';
+  description: string;
+  pictureUri: string;
+  duration: number;
+  nextSessionDate?: Date;
+
+  offer: Offer;
+
+  prerequisites: string[];
+  benefits: Benefit[];
+  agenda: Agenda;
+}
+
+export type Offer = EarlyBirdOffer | LastMinuteOffer;
+
+export interface EarlyBirdOffer {
+  type: 'early-bird';
+  price: number;
+  originalPrice: number;
+}
+
+export interface LastMinuteOffer {
+  type: 'last-minute';
+  price: number;
+}
+
+export interface Benefit {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export interface Agenda {
+  sections: AgendaSection[];
+}
+
+export interface AgendaSection {
+  title: string;
+  items: string[];
+}
+
+export function createWorkshop(workshop: Workshop): Workshop {
+  return workshop;
+}
