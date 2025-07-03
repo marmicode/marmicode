578e82f5a97945f4d9a95a6de933dd29a5e1887e
Younes Jaaidi <younes@marmicode.io>
2025-07-04 01:03:42 +0200

    feat: ✨ add workshops link to toolbar & unclutter it

--- libs/shared/router-helpers/lib/workshop-router-helper.ts
+++ libs/shared/router-helpers/lib/workshop-router-helper.ts
@@ -1,6 +1,10 @@
 export const workshopRouterHelper = {
   WORKSHOP_DETAIL_PATH: 'workshops/:workshopId',
   WORKSHOP_LIST_PATH: 'workshops',
-  detail: (workshopId: string) =>
-    `/workshops/${encodeURIComponent(workshopId)}`,
+  detail(workshopId: string) {
+    return `/workshops/${encodeURIComponent(workshopId)}`;
+  },
+  list() {
+    return '/workshops';
+  },
 };
