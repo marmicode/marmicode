f7d5bb18699af0ac717b83c01e10a2fbdd9e2ea4
Younes Jaaidi <younes@marmicode.io>
2025-08-27 23:33:14 +0200

    feat(shared): change cards styling

--- libs/shared/ui/lib/card.ng.ts
+++ libs/shared/ui/lib/card.ng.ts
@@ -7,15 +7,15 @@ import { MatIconModule } from '@angular/material/icon';
   selector: 'mc-card',
   imports: [MatCardModule, MatIconModule],
   template: ` <mat-card class="card">
-    <div class="title">
+    <div class="header" [class.has-icon]="icon() != null">
+      <h3 class="title zoom-on-card-hover">
+        <ng-content select="[slot='title']" />
+      </h3>
       @if (icon()) {
-        <div class="icon-container">
+        <div class="icon-container zoom-on-card-hover">
           <mat-icon class="icon" color="primary" [fontIcon]="icon()"></mat-icon>
         </div>
       }
-      <h3>
-        <ng-content select="[slot='title']" />
-      </h3>
     </div>
     <mat-card-content class="content">
       <ng-content select="[slot='content']" />
@@ -27,33 +27,57 @@ import { MatIconModule } from '@angular/material/icon';
       min-height: 150px;
       width: min(500px, 90vw);
       text-align: center;
-      transition: box-shadow 0.3s ease-in-out;
+      transition: box-shadow 0.2s ease-in-out;
+      overflow: hidden;
 
       &:hover {
         box-shadow:
           rgba(0, 0, 0, 0.25) 0px 3px 3px -1px,
           rgba(0, 0, 0, 0.2) 0px 5px 10px 0px;
       }
+
+      &:hover .zoom-on-card-hover {
+        transform: scale(1.2);
+      }
     }
 
-    .title {
+    .header {
+      position: relative;
+
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
-      margin-top: 1rem;
+
+      background: linear-gradient(135deg, #6a125dc4 0%, #53e0d9 100%);
+
+      padding: 1rem 0;
+      margin-bottom: 1rem;
+    }
+
+    .header.has-icon {
+      padding: 2rem 0 3rem 0;
+      margin-bottom: 45px;
     }
 
     .icon-container {
+      position: absolute;
+      bottom: -35px;
       width: 70px;
       height: 70px;
-      border-radius: 12px;
-      margin-bottom: 0.5rem;
-      border: 1px solid #e3cff0;
+
+      border-radius: 50%;
+      border: 1px solid #4a7a82;
+      background: white;
 
       display: flex;
       align-items: center;
       justify-content: center;
+    }
+
+    .title,
+    .icon-container {
+      transition: transform 0.2s ease-in-out;
     }
 
     .icon {
@@ -61,12 +85,12 @@ import { MatIconModule } from '@angular/material/icon';
       transform: scale(2.3);
     }
 
-    h3 {
+    .title {
+      color: white;
       font-weight: 700;
       font-size: 24px;
-      color: #111827;
       line-height: 1.4;
-      margin-bottom: 0.5em;
+      margin: 0;
     }
 
     .content {
