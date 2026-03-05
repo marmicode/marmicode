870f4b230338d7c8b11b18a2784eb11bd28a4931
Younes Jaaidi <younes@marmicode.io>
2026-03-05 23:05:33 +0100

    feat(workshop): ✨ add link to other languages

--- libs/workshop/ui-detail/workshop-hero.ng.ts
+++ libs/workshop/ui-detail/workshop-hero.ng.ts
@@ -13,7 +13,8 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-import { Hero } from '@marmicode/shared/ui';
+import { workshopRouterHelper } from '@marmicode/shared/router-helpers';
+import { Hero, LinkComponent } from '@marmicode/shared/ui';
 import { Workshop } from '@marmicode/workshop/core';
 import { formatPrice } from '@marmicode/workshop/ui';
 import { WORKSHOP_DETAIL_LABELS } from './workshop-detail.i18n';
@@ -24,6 +25,7 @@ import { UPCOMING_SESSIONS_SECTION_ID } from './workshop-sessions.ng';
   selector: 'mc-workshop-hero',
   imports: [
     Hero,
+    LinkComponent,
     MatButtonModule,
     MatIconModule,
     NgPlural,
@@ -55,36 +57,57 @@ import { UPCOMING_SESSIONS_SECTION_ID } from './workshop-sessions.ng';
             <span>{{ line }}</span>
           }
         </p>
-        <div class="actions">
-          @if (workshop().waitlistUrl; as waitlistUrl) {
-            <a [href]="waitlistUrl" matButton="filled" target="_blank">
-              <mat-icon>notifications</mat-icon>
-              {{ labels().joinWaitlist | uppercase }}
-            </a>
-          } @else {
+
+        <div class="actions-container">
+          <div class="actions">
+            @if (workshop().waitlistUrl; as waitlistUrl) {
+              <a [href]="waitlistUrl" matButton="filled" target="_blank">
+                <mat-icon>notifications</mat-icon>
+                {{ labels().joinWaitlist | uppercase }}
+              </a>
+            } @else {
+              <a
+                [routerLink]="[]"
+                [fragment]="upcomingSessionsSectionId"
+                matButton="filled"
+              >
+                <mat-icon>calendar_month</mat-icon>
+                {{ labels().bookASession | uppercase }}
+              </a>
+            }
             <a
-              [routerLink]="[]"
-              [fragment]="upcomingSessionsSectionId"
-              matButton="filled"
+              [href]="workshop().customSessionRequestUrl"
+              class="secondary"
+              matButton
+              target="_blank"
             >
-              <mat-icon>calendar_month</mat-icon>
-              {{ labels().bookASession | uppercase }}
+              <mat-icon>build</mat-icon>
+              {{ labels().requestACustomSession | uppercase }}
             </a>
-          }
-          <a
-            [href]="workshop().customSessionRequestUrl"
-            class="secondary"
-            matButton
-            target="_blank"
-          >
-            <mat-icon>build</mat-icon>
-            {{ labels().requestACustomSession | uppercase }}
-          </a>
-        </div>
-        @if (workshop().language === 'fr') {
-          <div class="bottom-note">
-            🇫🇷 Formation finançable jusqu'à 100% via OPCO.
           </div>
+
+          @for (alternate of alternateWorkshops(); track alternate) {
+            <mc-link
+              class="actions-note"
+              color="white"
+              [route]="alternate.route"
+            >
+              @switch (alternate.language) {
+                @case ('en') {
+                  🇬🇧 Also available in english
+                }
+                @case ('fr') {
+                  🇫🇷 Également disponible en français
+                }
+              }
+            </mc-link>
+          }
+        </div>
+
+        @if (workshop().language === 'fr') {
+          <p class="bottom-note">
+            🇫🇷 Formation finançable jusqu'à 100% via OPCO.
+          </p>
         }
       </ng-content>
     </mc-hero>
@@ -130,30 +153,41 @@ import { UPCOMING_SESSIONS_SECTION_ID } from './workshop-sessions.ng';
       line-height: 1.5;
     }
 
+    .actions-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin-bottom: 2rem;
+    }
+
     .actions {
       display: flex;
       align-items: center;
       gap: 1rem;
-      margin-bottom: 2rem;
 
       @media (max-width: 599.98px) {
         flex-direction: column;
         align-items: stretch;
       }
+
+      & a {
+        flex: 1 0 auto;
+        padding: 1.2rem;
+        border: none;
+        border-radius: 8px;
+        cursor: pointer;
+        font-size: 1em;
+
+        &.secondary {
+          background: rgba(255, 255, 255, 0.05);
+          box-shadow: 10px 10px 40px rgba(0, 0, 0, 0.2);
+        }
+      }
     }
 
-    .actions a {
-      flex: 1 0 auto;
-      padding: 1.2rem;
-      border: none;
-      border-radius: 8px;
-      cursor: pointer;
-      font-size: 1em;
-
-      &.secondary {
-        background: rgba(255, 255, 255, 0.05);
-        box-shadow: 10px 10px 40px rgba(0, 0, 0, 0.2);
-      }
+    .actions-note {
+      display: block;
+      margin: 1rem 0;
     }
 
     .bottom-note {
@@ -161,11 +195,23 @@ import { UPCOMING_SESSIONS_SECTION_ID } from './workshop-sessions.ng';
       font-size: 1.3rem;
       text-align: center;
       margin-bottom: 2rem;
+
+      .low-priority {
+        font-size: 1rem;
+        font-style: italic;
+      }
     }
   `,
 })
 export class WorkshopHero {
   workshop = input.required<Workshop>();
+
+  alternateWorkshops = computed(() =>
+    this.workshop().alternates?.map((alternate) => ({
+      language: alternate.language,
+      route: workshopRouterHelper.detail(alternate.id),
+    })),
+  );
   labels = computed(() => WORKSHOP_DETAIL_LABELS[this.workshop().language]);
   upcomingSessionsSectionId = UPCOMING_SESSIONS_SECTION_ID;
   offerType = computed(() => {
