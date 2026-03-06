6435c4563e1d64368fb2c8d30b87789b7330d82a
Younes Jaaidi <younes@marmicode.io>
2026-03-06 10:24:50 +0100

    feat(workshop): ✨ shorten pragmatic angular testing workshop url

--- libs/workshop/infra/workshops/pragmatic-angular-testing.shared.ts
+++ libs/workshop/infra/workshops/pragmatic-angular-testing.shared.ts
@@ -1,5 +1,5 @@
 export const PRAGMATIC_ANGULAR_TESTING_FULL_COURSE_ID =
-  'pragmatic-angular-testing-full-course';
+  'pragmatic-angular-testing';
 
 export const PRAGMATIC_ANGULAR_TESTING_FULL_COURSE_FR_ID =
   'test-angular-pragmatique';
